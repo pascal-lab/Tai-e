2363aef28cc7fbb6cee9d346c440cd56812e7c4c
Tian Tan <silverbullettt@gmail.com>
2020-02-21 23:37:01 +0800

    Refactor heap model implementation

--- src/main/java/sa/pta/analysis/heap/AllocationSiteBasedModel.java
+++ src/main/java/sa/pta/analysis/heap/AllocationSiteBasedModel.java
@@ -16,47 +16,4 @@ public class AllocationSiteBasedModel implements HeapModel {
         return objects.computeIfAbsent(allocationSite,
                 (k) -> new ObjImpl(allocationSite, type, containerMethod));
     }
-
-    private class ObjImpl implements Obj {
-
-        private final Object allocation;
-
-        private final Type type;
-
-        private final Method containerMethod;
-
-        private ObjImpl(Object allocation, Type type, Method containerMethod) {
-            this.allocation = allocation;
-            this.type = type;
-            this.containerMethod = containerMethod;
-        }
-
-        @Override
-        public Object getAllocationSite() {
-            return allocation;
-        }
-
-        @Override
-        public Type getType() {
-            return type;
-        }
-
-        @Override
-        public Method getContainerMethod() {
-            return containerMethod;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            ObjImpl obj = (ObjImpl) o;
-            return allocation.equals(obj.allocation);
-        }
-
-        @Override
-        public int hashCode() {
-            return allocation.hashCode();
-        }
-    }
 }
---
+++ src/main/java/sa/pta/analysis/heap/ObjImpl.java
@@ -0,0 +1,51 @@
+package sa.pta.analysis.heap;
+
+import sa.pta.element.Method;
+import sa.pta.element.Obj;
+import sa.pta.element.Type;
+
+/**
+ * A general implementation of Obj.
+ */
+class ObjImpl implements Obj {
+
+    private final Object allocation;
+
+    private final Type type;
+
+    private final Method containerMethod;
+
+    ObjImpl(Object allocation, Type type, Method containerMethod) {
+        this.allocation = allocation;
+        this.type = type;
+        this.containerMethod = containerMethod;
+    }
+
+    @Override
+    public Object getAllocationSite() {
+        return allocation;
+    }
+
+    @Override
+    public Type getType() {
+        return type;
+    }
+
+    @Override
+    public Method getContainerMethod() {
+        return containerMethod;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ObjImpl obj = (ObjImpl) o;
+        return allocation.equals(obj.allocation);
+    }
+
+    @Override
+    public int hashCode() {
+        return allocation.hashCode();
+    }
+}
