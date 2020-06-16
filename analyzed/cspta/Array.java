bc2b49b4b031809990ed0ff38785305c8d6c085f
Tian Tan <silverbullettt@gmail.com>
2020-06-17 00:51:28 +0800

    Add array test

---
+++ analyzed/cspta/Array.java
@@ -0,0 +1,11 @@
+class Array {
+
+    public static void main(String[] args) {
+        A[] arr = new A[10];
+        arr[0] = new A();
+        arr[1] = new A();
+        A a = arr[0];
+    }
+}
+
+class A {}
