b71d923c44169f31b0e40458d5c7dcc2e39df68b
Tian Tan <silverbullettt@gmail.com>
2020-07-06 21:12:38 +0800

    Add line counter

---
+++ linecounter.py
@@ -0,0 +1,30 @@
+import os, sys
+
+def countLine(filePath):
+	f = open(filePath)
+	l = len(f.readlines())
+	f.close()
+	return l
+
+def notExcluded(path, excludes):
+	for exc in excludes:
+		if exc in path:
+			return False
+	else:
+		return True
+
+if __name__ == '__main__':
+	excludes = [ 'test' ] + sys.argv[1:]
+	total = 0
+	nFile = 0
+	for root, dirs, files in os.walk(os.getcwd()):
+		for f in files:
+			path = os.path.join(root, f)
+			ext = os.path.splitext(path)[1]
+			if ext == '.java': 
+				if notExcluded(path, excludes):
+					l = countLine(path)
+					print('%s: %d' % (path, l))
+					total += l
+					nFile += 1
+	print('%d file, total line number: %d' % (nFile, total))
