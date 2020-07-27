bb818146279bc88c5bc40b83562ed04dc9f063cb
Tian Tan <silverbullettt@gmail.com>
2020-07-27 21:15:10 +0800

    Let Bamboo prefer .class input

--- src/main/java/bamboo/pta/Main.java
+++ src/main/java/bamboo/pta/Main.java
@@ -22,7 +22,6 @@ public class Main {
 
     public static void main(String[] args) {
         // Set options
-        Options.v().set_src_prec(Options.src_prec_java);
         Options.v().set_output_format(Options.output_format_jimple);
         Options.v().set_keep_line_number(true);
         Options.v().set_prepend_classpath(true);
