c451fd15af5f649bf1864a6b1819ca8710c9ad30
Tian Tan <silverbullettt@gmail.com>
2021-03-04 20:22:36 +0800

    Add new expressions

---
+++ src/main/java/pascal/taie/ir/NewInstance.java
@@ -0,0 +1,38 @@
+/*
+ * Tai-e: A Program Analysis Framework for Java
+ *
+ * Copyright (C) 2020 Tian Tan <tiantan@nju.edu.cn>
+ * Copyright (C) 2020 Yue Li <yueli@nju.edu.cn>
+ * All rights reserved.
+ *
+ * This software is designed for the "Static Program Analysis" course at
+ * Nanjing University, and it supports a subset of Java features.
+ * Tai-e is only for educational and academic purposes, and any form of
+ * commercial use is disallowed.
+ */
+
+package pascal.taie.ir;
+
+import pascal.taie.java.types.ClassType;
+
+/**
+ * Representation of new instance expression, e.g., new T.
+ */
+public class NewInstance extends NewExp {
+
+    private final ClassType type;
+
+    public NewInstance(ClassType type) {
+        this.type = type;
+    }
+
+    @Override
+    public ClassType getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return "new " + type;
+    }
+}
---
+++ src/main/java/pascal/taie/ir/NewMultiArray.java
@@ -0,0 +1,63 @@
+/*
+ * Tai-e: A Program Analysis Framework for Java
+ *
+ * Copyright (C) 2020 Tian Tan <tiantan@nju.edu.cn>
+ * Copyright (C) 2020 Yue Li <yueli@nju.edu.cn>
+ * All rights reserved.
+ *
+ * This software is designed for the "Static Program Analysis" course at
+ * Nanjing University, and it supports a subset of Java features.
+ * Tai-e is only for educational and academic purposes, and any form of
+ * commercial use is disallowed.
+ */
+
+package pascal.taie.ir;
+
+import pascal.taie.java.types.ArrayType;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Representation of new multi-array expression, e.g., new T[..][..][..].
+ */
+public class NewMultiArray extends NewExp {
+
+    private final ArrayType type;
+
+    private final List<Atom> lengths;
+
+    public NewMultiArray(ArrayType type, List<Atom> lengths) {
+        this.type = type;
+        this.lengths = Collections.unmodifiableList(lengths);
+    }
+
+    @Override
+    public ArrayType getType() {
+        return type;
+    }
+
+    public int getLengthCount() {
+        return lengths.size();
+    }
+
+    public Atom getLength(int i) {
+        return lengths.get(i);
+    }
+
+    public List<Atom> getLengths() {
+        return lengths;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("new ");
+        sb.append(type.getBaseType());
+        lengths.forEach(length ->
+                sb.append('[').append(length).append(']'));
+        for (int i = lengths.size(); i < type.getDimensions(); ++i) {
+            sb.append("[]");
+        }
+        return sb.toString();
+    }
+}
