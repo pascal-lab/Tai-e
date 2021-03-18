cf823fd316349cb9152eba83f06fd7d7fd0151a8
Yue Li <rogerlee0201@gmail.com>
2021-03-18 19:05:58 +0800

    bcel package added

---
+++ src/main/java/pascal/taie/frontend/bcel/package-info.java
@@ -0,0 +1,16 @@
+/*
+ * Tai-e: A Program Analysis Framework for Java
+ *
+ * Copyright (C) 2020 Tian Tan <tiantan@nju.edu.cn>
+ * Copyright (C) 2020 Yue Li <yueli@nju.edu.cn>
+ * All rights reserved.
+ *
+ * This software is designed for the "Static Program Analysis" course at
+ * Nanjing University, and it supports a subset of Java features.
+ * Tai-e is only for educational and academic purposes, and any form of
+ * commercial use is disallowed.
+ */
+/**
+ * TODO: We'll add another frontend implemented based on BCEL
+ */
+package pascal.taie.frontend.bcel;
