70a2e272a592cf09350deb5239c873734ebd4d15
Yue Li <rogerlee0201@gmail.com>
2021-03-18 19:23:17 +0800

    copyright updated

--- src/main/java/pascal/taie/frontend/soot/Modifiers.java
+++ src/main/java/pascal/taie/frontend/soot/Modifiers.java
@@ -1,14 +1,13 @@
 /*
- * Tai-e: A Program Analysis Framework for Java
+ * Tai-e: A Static Analysis Framework for Java
  *
- * Copyright (C) 2020 Tian Tan <tiantan@nju.edu.cn>
- * Copyright (C) 2020 Yue Li <yueli@nju.edu.cn>
+ * Copyright (C) 2020-- Tian Tan <tiantan@nju.edu.cn>
+ * Copyright (C) 2020-- Yue Li <yueli@nju.edu.cn>
  * All rights reserved.
  *
- * This software is designed for the "Static Program Analysis" course at
- * Nanjing University, and it supports a subset of Java features.
- * Tai-e is only for educational and academic purposes, and any form of
- * commercial use is disallowed.
+ * Tai-e is only for educational and academic purposes,
+ * and any form of commercial use is disallowed.
+ * Distribution of Tai-e is disallowed without the approval.
  */
 
 package pascal.taie.frontend.soot;
