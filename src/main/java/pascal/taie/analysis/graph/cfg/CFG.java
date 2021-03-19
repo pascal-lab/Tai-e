c05deba8bd95d2640e3bd121f553b2f77c36c119
Tian Tan <silverbullettt@gmail.com>
2021-03-19 23:16:42 +0800

    Initially add CFG

---
+++ src/main/java/pascal/taie/analysis/graph/cfg/CFG.java
@@ -0,0 +1,36 @@
+/*
+ * Tai-e: A Static Analysis Framework for Java
+ *
+ * Copyright (C) 2020-- Tian Tan <tiantan@nju.edu.cn>
+ * Copyright (C) 2020-- Yue Li <yueli@nju.edu.cn>
+ * All rights reserved.
+ *
+ * Tai-e is only for educational and academic purposes,
+ * and any form of commercial use is disallowed.
+ * Distribution of Tai-e is disallowed without the approval.
+ */
+
+package pascal.taie.analysis.graph.cfg;
+
+import pascal.taie.ir.IR;
+import pascal.taie.language.classes.JMethod;
+import pascal.taie.util.graph.Graph;
+
+import java.util.stream.Stream;
+
+/**
+ * Representation of a control-flow graph of a method.
+ * @param <N> type of nodes.
+ */
+public interface CFG<N> extends Graph<N> {
+
+    default JMethod getMethod() {
+        return getIR().getMethod();
+    }
+
+    IR getIR();
+
+    N getEntry();
+
+    Stream<N> getExits();
+}
