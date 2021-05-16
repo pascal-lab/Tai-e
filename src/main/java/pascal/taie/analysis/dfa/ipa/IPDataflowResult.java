a4a6bc05f8cb2a89f6c76524a553e469ff970a6c
Tian Tan <silverbullettt@gmail.com>
2021-05-17 03:43:08 +0800

    Add new inter-procedural work-list solver, to be tested

--- src/main/java/pascal/taie/analysis/dfa/ipa/IPDataflowResult.java
+++ src/main/java/pascal/taie/analysis/dfa/ipa/IPDataflowResult.java
@@ -10,30 +10,29 @@
  * Distribution of Tai-e is disallowed without the approval.
  */
 
-package pascal.taie.analysis.dfa.fact;
+package pascal.taie.analysis.dfa.ipa;
 
-import pascal.taie.analysis.graph.cfg.Edge;
+import pascal.taie.analysis.dfa.fact.NodeResult;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
- * Object for managing the data-flow facts associated with nodes and edges of a CFG.
- * @param <Node> type of CFG nodes
- * @param <Fact> type of data-flow facts
+ * TODO: merge with {@link pascal.taie.analysis.dfa.fact.DataflowResult}
+ *  by parameterize edge type?
  */
-public class DataflowResult<Node, Fact> extends NodeResult<Node, Fact> {
+public class IPDataflowResult<Node, Fact> extends NodeResult<Node, Fact> {
 
-    private final Map<Edge<Node>, Fact> edgeFacts = new LinkedHashMap<>();
+    private final Map<ICFGEdge<Node>, Fact> edgeFacts = new LinkedHashMap<>();
 
     /**
      * @return the fact of given edge.
      */
-    public Fact getEdgeFact(Edge<Node> edge) {
+    public Fact getEdgeFact(ICFGEdge<Node> edge) {
         return edgeFacts.get(edge);
     }
 
-    public void setEdgeFact(Edge<Node> edge, Fact fact) {
+    public void setEdgeFact(ICFGEdge<Node> edge, Fact fact) {
         edgeFacts.put(edge, fact);
     }
 }
