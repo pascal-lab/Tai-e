2f63b5bf7db51d51b4a659e7e9feef1e9890090b
lancetyyy <lancetyyy@qq.com>
2021-05-17 16:35:44 +0800

    BSMCallEdge

---
+++ src/main/java/pascal/taie/analysis/pta/plugin/invokedynamic/BSMCallEdge.java
@@ -0,0 +1,27 @@
+package pascal.taie.analysis.pta.plugin.invokedynamic;
+
+import pascal.taie.analysis.graph.callgraph.CallKind;
+import pascal.taie.analysis.graph.callgraph.Edge;
+import pascal.taie.analysis.oldpta.ir.Call;
+import pascal.taie.analysis.pta.core.cs.element.CSCallSite;
+import pascal.taie.analysis.pta.core.cs.element.CSMethod;
+import pascal.taie.language.classes.JClass;
+
+public class BSMCallEdge  extends Edge<CSCallSite, CSMethod> {
+
+    private JClass lookupClass;
+
+    public BSMCallEdge(CSCallSite csCallSite, CSMethod callee, JClass lookupClass) {
+        super(CallKind.OTHER, csCallSite, callee);
+        this.lookupClass = lookupClass;
+    }
+
+    public JClass getLookupClass() {
+        return lookupClass;
+    }
+
+    public void setLookupClass(JClass lookupClass) {
+        this.lookupClass = lookupClass;
+    }
+
+}
