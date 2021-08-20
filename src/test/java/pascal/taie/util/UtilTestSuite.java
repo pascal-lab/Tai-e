a2788cfdce9864a4d38db4236cec1f3cc4c684d6
Tian Tan <silverbullettt@gmail.com>
2021-08-20 15:31:38 +0800

    Implement IndexMap and add test cases for it

--- src/test/java/pascal/taie/util/UtilTestSuite.java
+++ src/test/java/pascal/taie/util/UtilTestSuite.java
@@ -19,6 +19,7 @@ import pascal.taie.util.collection.ArraySetTest;
 import pascal.taie.util.collection.CollectionUtilsTest;
 import pascal.taie.util.collection.HybridArrayHashMapTest;
 import pascal.taie.util.collection.HybridArrayHashSetTest;
+import pascal.taie.util.collection.IndexMapTest;
 import pascal.taie.util.collection.SetQueueTest;
 import pascal.taie.util.graph.GraphTest;
 
@@ -29,6 +30,7 @@ import pascal.taie.util.graph.GraphTest;
         ArrayMapTest.class,
         HybridArrayHashMapTest.class,
         HybridArrayHashSetTest.class,
+        IndexMapTest.class,
         SetQueueTest.class,
         CollectionUtilsTest.class,
         // graph
