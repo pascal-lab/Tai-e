00fd5e41062f2e548566212ff78c4ad00d4cac95
Tian Tan <silverbullettt@gmail.com>
2021-10-09 22:50:50 +0800

    Update test case SimpleBinary

--- src/test/resources/dataflow/constprop/SimpleBinary.java
+++ src/test/resources/dataflow/constprop/SimpleBinary.java
@@ -1,7 +1,32 @@
 class SimpleBinary {
 
-    void constant() {
+    int arithmetic() {
         int x = 1, y = 2;
-        int a = x + y;
+        int z = x + y;
+        return z;
+    }
+
+    boolean condition() {
+        int x = 1, y = 2;
+        boolean z = x == y;
+        return z;
+    }
+    
+    int shift() {
+        int x = 1, y = 2;
+        int z = x << y;
+        return z;
+    }
+
+    int bitwise() {
+        int x = 1, y = 2;
+        int z = x | y;
+        return z;
+    }
+
+    int nac(int p) {
+        int x = 1, y = p;
+        int z = x * y;
+        return z;
     }
 }
