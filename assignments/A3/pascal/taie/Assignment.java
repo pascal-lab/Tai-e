a7c8f6c6c2b65c74871b98aa1b3356ba7a7d4470
Tian Tan <silverbullettt@gmail.com>
2021-10-18 00:11:12 +0800

    Recover Assignment to no-time-budget version

--- assignments/A3/pascal/taie/Assignment.java
+++ assignments/A3/pascal/taie/Assignment.java
@@ -12,29 +12,16 @@
 
 package pascal.taie;
 
-import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 /**
  * Main class for assignments.
  */
 public class Assignment {
 
-    private static final long TIME_BUDGET = 20;
-
     public static void main(String[] args) {
-        runWithTimeout(() -> run(args), TIME_BUDGET);
-    }
-
-    private static void run(String[] args) {
         if (args.length > 0) {
             List<String> argList = new ArrayList<>();
             Collections.addAll(argList, "-pp", "-p", "plan.yml");
@@ -42,24 +29,6 @@ public class Assignment {
             Main.main(argList.toArray(new String[0]));
         } else {
             System.out.println("Usage: -cp <CLASS_PATH> -m <CLASS_NAME>");
-        }
-    }
-
-    private static void runWithTimeout(Runnable task, long seconds) {
-        Duration timeout = Duration.ofSeconds(seconds);
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        @SuppressWarnings("unchecked")
-        Future<Void> handler = (Future<Void>) executor.submit(task);
-        try {
-            handler.get(timeout.getSeconds(), TimeUnit.SECONDS);
-        } catch (TimeoutException e) {
-            e.printStackTrace();
-            System.exit(1);
-        } catch (InterruptedException | ExecutionException e) {
-            e.getCause().printStackTrace();
-            System.exit(1);
-        } finally {
-            executor.shutdown();
         }
     }
 }
