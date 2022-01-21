b11e5d6de71b90c194a9081655c129baf85665e9
Tian Tan <silverbullettt@gmail.com>
2022-01-21 22:14:37 +0800

    Add various annotation elements

---
+++ src/main/java/pascal/taie/language/annotation/ClassElement.java
@@ -0,0 +1,17 @@
+/*
+ * Tai-e: A Static Analysis Framework for Java
+ *
+ * Copyright (C) 2020-- Tian Tan <tiantan@nju.edu.cn>
+ * Copyright (C) 2020-- Yue Li <yueli@nju.edu.cn>
+ * All rights reserved.
+ *
+ * Tai-e is only for educational and academic purposes,
+ * and any form of commercial use is disallowed.
+ * Distribution of Tai-e is disallowed without the approval.
+ */
+
+package pascal.taie.language.annotation;
+
+// TODO: resolve classDescriptor to Type?
+public record ClassElement(String classDescriptor) implements Element {
+}
