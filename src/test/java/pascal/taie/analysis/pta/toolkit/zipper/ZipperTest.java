e4d438954a8c017c2c3f4ae0abac0e41889e4f67
Tian Tan <silverbullettt@gmail.com>
2022-05-12 20:57:34 +0800

    Add tests for OAG and OFG

---
+++ src/test/java/pascal/taie/analysis/pta/toolkit/zipper/ZipperTest.java
@@ -0,0 +1,89 @@
+/*
+ * Tai-e: A Static Analysis Framework for Java
+ *
+ * Copyright (C) 2022 Tian Tan <tiantan@nju.edu.cn>
+ * Copyright (C) 2022 Yue Li <yueli@nju.edu.cn>
+ *
+ * This file is part of Tai-e.
+ *
+ * Tai-e is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Tai-e is distributed in the hope that it will be useful,but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Tai-e. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package pascal.taie.analysis.pta.toolkit.zipper;
+
+import org.junit.Test;
+import pascal.taie.World;
+import pascal.taie.analysis.Tests;
+import pascal.taie.analysis.pta.PointerAnalysis;
+import pascal.taie.analysis.pta.PointerAnalysisResult;
+import pascal.taie.analysis.pta.core.heap.Obj;
+import pascal.taie.analysis.pta.toolkit.PointerAnalysisResultExImpl;
+import pascal.taie.util.graph.DotDumper;
+
+import java.util.Map;
+
+public class ZipperTest {
+
+    private static final String CS = "contextsensitivity";
+
+    private static final String BASIC = "basic";
+
+    @Test
+    public void testOAG() {
+        dumpOAG(CS, "TwoObject", "cs:2-obj");
+    }
+
+    private static void dumpOAG(String dir, String main, String opts) {
+        Tests.testPTA(dir, main,  opts);
+        PointerAnalysisResult pta = World.get().getResult(PointerAnalysis.ID);
+        ObjectAllocationGraph oag = new ObjectAllocationGraph(
+            new PointerAnalysisResultExImpl(pta));
+        new DotDumper<Obj>().dump(oag, "output/" + main + "-oag.dot");
+    }
+
+    @Test
+    public void testOFG() {
+        dumpOFG(BASIC, "Cast");
+        dumpOFG(BASIC, "StoreLoad");
+        dumpOFG(BASIC, "Array");
+        dumpOFG(BASIC, "CallParamRet");
+    }
+
+    private static void dumpOFG(String dir, String main) {
+        Tests.testPTA(dir, main);
+        PointerAnalysisResult pta = World.get().getResult(PointerAnalysis.ID);
+        ObjectFlowGraph ofg = new ObjectFlowGraph(pta);
+        new DotDumper<OFGNode>()
+            .setNodeAttributes(n -> {
+                if (n instanceof InstanceFieldNode) {
+                    return Map.of("shape", "box");
+                } else if (n instanceof ArrayIndexNode) {
+                    return Map.of("shape", "box",
+                        "style", "filled", "color", "grey");
+                } else { // VarNode
+                    return Map.of();
+                }
+            })
+            .setEdgeAttrs(e -> {
+                OFGEdge edge = (OFGEdge) e;
+                return switch (edge.kind()) {
+                    case INTERPROCEDURAL_ASSIGN -> Map.of("color", "blue");
+                    case INSTANCE_STORE -> Map.of("color", "red");
+                    case INSTANCE_LOAD -> Map.of("style", "dashed", "color", "red");
+                    default -> Map.of();
+                };
+            })
+            .dump(ofg, "output/" + main + "-ofg.dot");
+    }
+}
