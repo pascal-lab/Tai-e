ed45dc20251af70f686c76f3cfd1be9ea966ae5c
Tian Tan <silverbullettt@gmail.com>
2022-05-23 21:36:32 +0800

    Add ConstraintChecker for pointer analysis solver

---
+++ src/main/java/pascal/taie/analysis/pta/plugin/ConstraintChecker.java
@@ -0,0 +1,62 @@
+/*
+ * Tai-e: A Static Analysis Framework for Java
+ *
+ * Copyright (C) 2022 Tian Tan <tiantan@nju.edu.cn>
+ * Copyright (C) 2022 Yue Li <yueli@nju.edu.cn>
+ *
+ * This file is part of Tai-e.
+ *
+ * Tai-e is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Tai-e is distributed in the hope that it will be useful,but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Tai-e. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package pascal.taie.analysis.pta.plugin;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import pascal.taie.analysis.pta.core.cs.element.CSVar;
+import pascal.taie.analysis.pta.pts.PointsToSet;
+import pascal.taie.ir.exp.Var;
+import pascal.taie.language.classes.JMethod;
+import pascal.taie.util.collection.Sets;
+
+import java.util.Set;
+
+/**
+ * This class is for debugging/testing purpose.
+ *
+ * {@link pascal.taie.analysis.pta.core.solver.Solver} needs to satisfy
+ * an important constraint that onNewMethod(m) must happen before
+ * onNewPointsToSet(v, pts) for any variables in m. This class checks
+ * the constraint and issues warnings when it is unsatisfied.
+ */
+public class ConstraintChecker implements Plugin {
+
+    private static final Logger logger = LogManager.getLogger(ConstraintChecker.class);
+
+    private final Set<JMethod> reached = Sets.newSet(4096);
+
+    @Override
+    public void onNewPointsToSet(CSVar csVar, PointsToSet pts) {
+        Var var = csVar.getVar();
+        if (!reached.contains(var.getMethod())) {
+            logger.warn("Warning: hit {} before processing {}",
+                var, var.getMethod());
+        }
+    }
+
+    @Override
+    public void onNewMethod(JMethod method) {
+        reached.add(method);
+    }
+}
