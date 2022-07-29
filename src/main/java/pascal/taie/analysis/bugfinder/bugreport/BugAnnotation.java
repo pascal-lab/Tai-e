21a9cb29ed32fb464a01e0cac44d90a99ebc47d4
Zhang Shumeng <1327657261@qq.com>
2022-07-30 01:37:04 +0800

    add detectors

--- src/main/java/pascal/taie/analysis/bugfinder/bugreport/BugAnnotation.java
+++
@@ -1,9 +0,0 @@
-package pascal.taie.analysis.bugfinder.bugreport;
-
-/**
- * Provide context information for a BugInstance
- */
-
-public interface BugAnnotation {
-    public String getDescription();
-}
--- src/main/java/pascal/taie/analysis/bugfinder/bugreport/BugReporter.java
+++
@@ -1,7 +0,0 @@
-//package pascal.taie.analysis.bugfinder.bugreport;
-//
-//public interface BugReporter {
-//
-//    public void reportBug(BugInstance bugInstance);
-//
-//}
