a366124965f0ecf6bf6cca395f0faf6f8b15ebc6
Tian Tan <silverbullettt@gmail.com>
2022-08-06 20:26:02 +0800

    Do not put one-object sets into mergeMap

--- src/main/java/pascal/taie/analysis/pta/toolkit/mahjong/MahjongHeapModel.java
+++ src/main/java/pascal/taie/analysis/pta/toolkit/mahjong/MahjongHeapModel.java
@@ -34,37 +34,41 @@ import pascal.taie.util.collection.Maps;
 
 import java.util.Collection;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 class MahjongHeapModel extends AbstractHeapModel {
 
     // currently, perform merging for only NewObj
-    private final Map<New, MergedObj> mergedObjs;
+    private final Map<New, MergedObj> mergeMap;
 
     MahjongHeapModel(AnalysisOptions options, Collection<Set<Obj>> objGroups) {
         super(options);
-        mergedObjs = buildMergeMap(objGroups);
+        mergeMap = buildMergeMap(objGroups);
     }
 
     private Map<New, MergedObj> buildMergeMap(Collection<Set<Obj>> objGroups) {
-        Map<New, MergedObj> mergedObjs = Maps.newMap();
-        objGroups.forEach(objs -> {
-            Type type = CollectionUtils.getOne(objs).getType();
-            MergedObj mergedObj = add(new MergedObj(type,
-                    "<Mahjong Merged + " + type + " >"));
-            objs.forEach(obj -> {
-                if (obj instanceof NewObj newObj) {
-                    New allocSite = newObj.getAllocation();
-                    mergedObjs.put(allocSite, mergedObj);
-                    mergedObj.addRepresentedObj(getNewObj(allocSite));
-                }
-            });
-        });
-        return mergedObjs;
+        Map<New, MergedObj> mergeMap = Maps.newMap();
+        objGroups.stream()
+                .filter(objs -> objs.size() > 1)
+                .forEach(objs -> {
+                    Type type = CollectionUtils.getOne(objs).getType();
+                    MergedObj mergedObj = add(new MergedObj(type,
+                            "<Mahjong-merged " + type + ">"));
+                    objs.forEach(obj -> {
+                        if (obj instanceof NewObj newObj) {
+                            New allocSite = newObj.getAllocation();
+                            mergeMap.put(allocSite, mergedObj);
+                            mergedObj.addRepresentedObj(getNewObj(allocSite));
+                        }
+                    });
+                });
+        return mergeMap;
     }
 
     @Override
     protected Obj doGetObj(New allocSite) {
-        return mergedObjs.get(allocSite);
+        return Objects.requireNonNullElse(mergeMap.get(allocSite),
+                getNewObj(allocSite));
     }
 }
