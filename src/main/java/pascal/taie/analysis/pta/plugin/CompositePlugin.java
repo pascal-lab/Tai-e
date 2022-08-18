b651973782a2ded69e6cbb8bfdd18110a0368c50
Tian Tan <silverbullettt@gmail.com>
2022-08-19 00:09:08 +0800

    Avoid calling plugins that do not overwrite specific methods declared in Plugin

--- src/main/java/pascal/taie/analysis/pta/plugin/CompositePlugin.java
+++ src/main/java/pascal/taie/analysis/pta/plugin/CompositePlugin.java
@@ -33,8 +33,8 @@ import pascal.taie.analysis.pta.pts.PointsToSet;
 import pascal.taie.ir.stmt.Invoke;
 import pascal.taie.language.classes.JMethod;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -43,49 +43,84 @@ import java.util.List;
  */
 public class CompositePlugin implements Plugin {
 
-    private final List<Plugin> plugins = new ArrayList<>();
+    private final List<Plugin> allPlugins = new ArrayList<>();
+
+    // Use separate lists to store plugins that overwrite specific method.
+
+    private final List<Plugin> onNewPointsToSetPlugins = new ArrayList<>();
+
+    private final List<Plugin> onNewCallEdgePlugins = new ArrayList<>();
+
+    private final List<Plugin> onNewMethodPlugins = new ArrayList<>();
+
+    private final List<Plugin> onNewCSMethodPlugins = new ArrayList<>();
+
+    private final List<Plugin> onUnresolvedCallPlugins = new ArrayList<>();
 
     public void addPlugin(Plugin... plugins) {
-        Collections.addAll(this.plugins, plugins);
+        for (Plugin plugin : plugins) {
+            allPlugins.add(plugin);
+            addPlugin(plugin, onNewPointsToSetPlugins,
+                    "onNewPointsToSet", CSVar.class, PointsToSet.class);
+            addPlugin(plugin, onNewCallEdgePlugins, "onNewCallEdge", Edge.class);
+            addPlugin(plugin, onNewMethodPlugins, "onNewMethod", JMethod.class);
+            addPlugin(plugin, onNewCSMethodPlugins, "onNewCSMethod", CSMethod.class);
+            addPlugin(plugin, onUnresolvedCallPlugins,
+                    "onUnresolvedCall", CSObj.class, Context.class, Invoke.class);
+        }
+    }
+
+    private void addPlugin(Plugin plugin, List<Plugin> plugins,
+                           String name, Class<?>... parameterTypes) {
+        try {
+            Method method = plugin.getClass().getMethod(name, parameterTypes);
+            if (!method.getDeclaringClass().equals(Plugin.class)) {
+                // the plugin overwrites the specific method
+                plugins.add(plugin);
+            }
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException("Can't find method '" + name +
+                    "' in " + plugin.getClass(), e);
+        }
     }
 
     @Override
     public void setSolver(Solver solver) {
-        plugins.forEach(p -> p.setSolver(solver));
+        allPlugins.forEach(p -> p.setSolver(solver));
     }
 
     @Override
     public void onStart() {
-        plugins.forEach(Plugin::onStart);
+        allPlugins.forEach(Plugin::onStart);
     }
 
     @Override
     public void onFinish() {
-        plugins.forEach(Plugin::onFinish);
+        allPlugins.forEach(Plugin::onFinish);
     }
 
     @Override
     public void onNewPointsToSet(CSVar csVar, PointsToSet pts) {
-        plugins.forEach(p -> p.onNewPointsToSet(csVar, pts));
+        onNewPointsToSetPlugins.forEach(p -> p.onNewPointsToSet(csVar, pts));
     }
 
     @Override
     public void onNewCallEdge(Edge<CSCallSite, CSMethod> edge) {
-        plugins.forEach(p -> p.onNewCallEdge(edge));
+        onNewCallEdgePlugins.forEach(p -> p.onNewCallEdge(edge));
     }
 
     @Override
     public void onNewMethod(JMethod method) {
-        plugins.forEach(p -> p.onNewMethod(method));
+        onNewMethodPlugins.forEach(p -> p.onNewMethod(method));
     }
 
     @Override
     public void onNewCSMethod(CSMethod csMethod) {
-        plugins.forEach(p -> p.onNewCSMethod(csMethod));
+        onNewCSMethodPlugins.forEach(p -> p.onNewCSMethod(csMethod));
     }
 
     @Override
     public void onUnresolvedCall(CSObj recv, Context context, Invoke invoke) {
-        plugins.forEach(p -> p.onUnresolvedCall(recv, context, invoke));
+        onUnresolvedCallPlugins.forEach(p -> p.onUnresolvedCall(recv, context, invoke));
     }
 }
