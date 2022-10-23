8b1c29c3e4d528c4d2890121e688848eb153e8db
Tian Tan <silverbullettt@gmail.com>
2022-10-23 10:23:35 +0800

    Refactor ProjectBuilder

--- src/main/java/pascal/taie/project/OptionsProjectBuilder.java
+++ src/main/java/pascal/taie/project/OptionsProjectBuilder.java
@@ -2,10 +2,33 @@ package pascal.taie.project;
 
 import pascal.taie.config.Options;
 
-public class OptionsProjectBuilder {
+import java.util.List;
 
-    public Project build(Options options) {
+public class OptionsProjectBuilder extends AbstractProjectBuilder {
+
+    private final Options options;
+
+    public OptionsProjectBuilder(Options options) {
+        this.options = options;
+    }
+
+    @Override
+    protected String getMainClass() {
         return null;
     }
 
+    @Override
+    protected int getJavaVersion() {
+        return 0;
+    }
+
+    @Override
+    protected List<String> getInputClasses() {
+        return null;
+    }
+
+    @Override
+    protected List<FileContainer> getRootContainers() {
+        return null;
+    }
 }
