98280ad449016ee56407a257516b27c6f4b97f41
Chenxi Li <ayanamists@gmail.com>
2022-10-28 17:29:53 +0800

    Implement DepResolver, now handle class file.

---
+++ src/main/java/pascal/taie/frontend/newfrontend/DepClassVisitor.java
@@ -0,0 +1,218 @@
+package pascal.taie.frontend.newfrontend;
+
+import org.objectweb.asm.*;
+import pascal.taie.util.collection.Sets;
+
+import java.util.Arrays;
+import java.util.Set;
+
+// TODO: 1. Add AnnotationVisitor
+//       2. Check generic
+public class DepClassVisitor extends ClassVisitor {
+
+    // TODO: handle Ldc
+    private class DepMethodVisitor extends MethodVisitor {
+
+        public DepMethodVisitor() {
+            super(Opcodes.ASM9);
+        }
+
+        @Override
+        public AnnotationVisitor visitAnnotation(String descriptor, boolean visible) {
+            addDescriptor(descriptor);
+            return null;
+        }
+
+
+        @Override
+        public AnnotationVisitor visitTypeAnnotation(
+                int typeRef,
+                TypePath typePath,
+                String descriptor,
+                boolean visible) {
+           addDescriptor(descriptor);
+           return null;
+        }
+
+        @Override
+        public void visitFieldInsn(
+                int opcode,
+                String owner,
+                String name,
+                String descriptor) {
+            addDescriptor(descriptor);
+        }
+
+        @Override
+        public AnnotationVisitor visitInsnAnnotation(
+                int typeRef,
+                TypePath typePath,
+                String descriptor,
+                boolean visible) {
+            addDescriptor(descriptor);
+            return null;
+        }
+
+        @Override
+        public void visitLocalVariable(
+                String name,
+                String descriptor,
+                String signature,
+                Label start, Label end, int index) {
+            addDescriptor(descriptor);
+        }
+
+        @Override
+        public AnnotationVisitor visitLocalVariableAnnotation(
+                int typeRef,
+                TypePath typePath,
+                Label[] start, Label[] end,
+                int[] index,
+                String descriptor,
+                boolean visible) {
+            addDescriptor(descriptor);
+            return null;
+        }
+
+        @Override
+        public void visitMethodInsn(
+                int opcode,
+                String owner,
+                String name,
+                String descriptor,
+                boolean isInterface) {
+            addDescriptor(descriptor);
+        }
+
+        @Override
+        public void visitMultiANewArrayInsn(String descriptor, int numDimensions) {
+            addDescriptor(descriptor);
+        }
+
+        @Override
+        public AnnotationVisitor visitParameterAnnotation(
+                int parameter,
+                String descriptor,
+                boolean visible) {
+            addDescriptor(descriptor);
+            return null;
+        }
+
+        @Override
+        public void	visitTryCatchBlock(
+                Label start, Label end, Label handler,
+                String type) {
+            addBinaryName(type);
+        }
+
+        @Override
+        public AnnotationVisitor visitTryCatchAnnotation(
+                int typeRef,
+                TypePath typePath,
+                String descriptor,
+                boolean visible) {
+            addDescriptor(descriptor);
+            return null;
+        }
+
+        @Override
+        public void visitTypeInsn(int opCode, String type) {
+            addBinaryName(type);
+        }
+
+    }
+
+    public Set<String> getBinaryNames() {
+        return binaryNames;
+    }
+
+    private final Set<String> binaryNames;
+
+    public DepClassVisitor() {
+        super(Opcodes.ASM9);
+        binaryNames = Sets.newSet();
+    }
+
+    @Override
+    public FieldVisitor visitField(
+            int access,
+            java.lang.String name,
+            java.lang.String descriptor,
+            java.lang.String signature,
+            java.lang.Object value) {
+        addDescriptor(descriptor);
+        return null;
+    }
+
+    @Override
+    public MethodVisitor visitMethod(
+            int access,
+            String name,
+            String descriptor,
+            String signature,
+            String[] exceptions) {
+        addMethodDescriptor(descriptor);
+
+        if (exceptions != null) {
+            binaryNames.addAll(Arrays.asList(exceptions));
+        }
+
+        return new DepMethodVisitor();
+    }
+
+    @Override
+    public AnnotationVisitor visitAnnotation(String descriptor, boolean visible) {
+        addDescriptor(descriptor);
+        return null;
+    }
+
+    @Override
+    public AnnotationVisitor visitTypeAnnotation(
+            int typeRef,
+            TypePath typePath,
+            String descriptor,
+            boolean visible) {
+        addDescriptor(descriptor);
+        return null;
+    }
+
+    @Override
+    public RecordComponentVisitor visitRecordComponent(
+            String name,
+            String descriptor,
+            String signature) {
+        addDescriptor(descriptor);
+        return null;
+    }
+
+    @Override
+    public void visitNestHost(String nestHost) {
+        binaryNames.add(nestHost);
+    }
+
+    @Override
+    public void visitNestMember(String member) {
+        binaryNames.add(member);
+    }
+
+    private void addBinaryName(String binaryName) {
+        binaryNames.add(binaryName);
+    }
+
+    private void addDescriptor(String descriptor) {
+        binaryNames.add(getBinaryName(descriptor));
+    }
+
+    private void addMethodDescriptor(String descriptor) {
+        Type type = Type.getType(descriptor);
+
+        for (var i : type.getArgumentTypes()) {
+            binaryNames.add(i.getClassName());
+        }
+        binaryNames.add(type.getReturnType().getClassName());
+    }
+
+    private static String getBinaryName(String descriptor) {
+        return Type.getType(descriptor).getClassName();
+    }
+}
--- src/main/java/pascal/taie/frontend/newfrontend/DepResolver.java
+++ src/main/java/pascal/taie/frontend/newfrontend/DepResolver.java
@@ -1,23 +1,79 @@
 package pascal.taie.frontend.newfrontend;
 
+import org.objectweb.asm.ClassReader;
+import pascal.taie.project.AnalysisFile;
+import pascal.taie.project.ClassFile;
+import pascal.taie.project.JavaSourceFile;
 import pascal.taie.project.Project;
+import pascal.taie.util.collection.Maps;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 public class DepResolver implements ClassResolver {
 
+    private final Map<String, ClassSource> sourceMap;
+
+    private Project project;
+
     public DepResolver() {
+        sourceMap = Maps.newHybridMap();
     }
 
     @Override
     public ClassSource getClassSource(String binaryName) {
-        return null;
+        return sourceMap.get(binaryName);
     }
 
     @Override
     public int getTotalClasses() {
-        return 0;
+        return sourceMap.size();
     }
 
     @Override
     public void resolve(Project p) {
+        String entry = p.getMainClass();
+        this.project = p;
+        try {
+            buildClosure(entry);
+        } catch (IOException e) {
+            // TODO: fail info
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void buildClosure(String binaryName) throws IOException {
+        if (sourceMap.containsKey(binaryName)) {
+            return;
+        }
+
+        AnalysisFile f = project.locate(binaryName);
+        if (f == null) {
+            throw new FileNotFoundException(binaryName);
+        }
+
+        List<String> deps = null;
+        if (f instanceof JavaSourceFile jFile) {
+            // TODO: fill here
+        } else if (f instanceof ClassFile cFile) {
+            deps = buildClassDeps(cFile);
+        } else {
+            throw new IllegalStateException();
+        }
+
+        for (var i : deps) {
+            buildClosure(i);
+        }
+    }
+
+    private List<String> buildClassDeps(ClassFile cFile) throws IOException {
+        byte[] content = cFile.resource().getContent();
+        ClassReader reader = new ClassReader(content);
+        DepClassVisitor v = new DepClassVisitor();
+        reader.accept(v, ClassReader.SKIP_CODE);
+        return v.getBinaryNames().stream().toList();
     }
 }
