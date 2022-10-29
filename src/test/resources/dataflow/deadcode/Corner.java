4a49558aee9daffb0d5596e2c97766655f03267c
Tian Tan <silverbullettt@gmail.com>
2022-10-29 21:35:51 +0800

    Update test case for DCD

--- src/test/resources/dataflow/deadcode/Corner.java
+++ src/test/resources/dataflow/deadcode/Corner.java
@@ -6,6 +6,16 @@ class Corner {
         return 5;
     }
 
+    void sideEffectMod() {
+        int x = 0;
+        int y = 1 % x;
+    }
+
+    void sideEffectDivide() {
+        int x = 0;
+        int y = 1 / x;
+    }
+
     int invoke() {
         int x = 1;
         int y = 2;
