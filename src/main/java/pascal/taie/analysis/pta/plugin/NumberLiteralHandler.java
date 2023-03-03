367a4587b2ebbe615fd4e423b2808c8a0f97a8bd
Tian Tan <silverbullettt@gmail.com>
2023-03-03 23:03:40 +0800

    Add NumberLiteralHandler and now pointer analysis can propagate primitive constants

---
+++ src/main/java/pascal/taie/analysis/pta/plugin/NumberLiteralHandler.java
@@ -0,0 +1,91 @@
+/*
+ * Tai-e: A Static Analysis Framework for Java
+ *
+ * Copyright (C) 2022 Tian Tan <tiantan@nju.edu.cn>
+ * Copyright (C) 2022 Yue Li <yueli@nju.edu.cn>
+ *
+ * This file is part of Tai-e.
+ *
+ * Tai-e is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Tai-e is distributed in the hope that it will be useful,but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Tai-e. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package pascal.taie.analysis.pta.plugin;
+
+import pascal.taie.analysis.pta.core.cs.context.Context;
+import pascal.taie.analysis.pta.core.cs.element.CSMethod;
+import pascal.taie.analysis.pta.core.heap.Descriptor;
+import pascal.taie.analysis.pta.core.heap.HeapModel;
+import pascal.taie.analysis.pta.core.heap.Obj;
+import pascal.taie.analysis.pta.core.solver.Solver;
+import pascal.taie.ir.exp.NumberLiteral;
+import pascal.taie.ir.exp.Var;
+import pascal.taie.ir.stmt.AssignLiteral;
+import pascal.taie.language.classes.JMethod;
+import pascal.taie.util.collection.Maps;
+import pascal.taie.util.collection.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Handles {@link AssignLiteral} where the RValue is of {@link NumberLiteral}.
+ */
+public class NumberLiteralHandler implements Plugin {
+
+    private static final Descriptor NUMBER_DESC = () -> "NumberObj";
+
+    private final Map<JMethod, List<Pair<Var, Number>>> assignMap = Maps.newMap();
+
+    private Solver solver;
+
+    private HeapModel heapModel;
+
+    private Context emptyCtx;
+
+    @Override
+    public void setSolver(Solver solver) {
+        this.solver = solver;
+        heapModel = solver.getHeapModel();
+        emptyCtx = solver.getContextSelector().getEmptyContext();
+    }
+
+    @Override
+    public void onNewMethod(JMethod method) {
+        List<Pair<Var, Number>> assigns = new ArrayList<>();
+        method.getIR().forEach(s -> {
+            if (s instanceof AssignLiteral assign &&
+                    assign.getRValue() instanceof NumberLiteral literal) {
+                assigns.add(new Pair<>(assign.getLValue(), literal.getNumber()));
+            }
+        });
+        if (!assigns.isEmpty()) {
+            assignMap.put(method, assigns);
+        }
+    }
+
+    @Override
+    public void onNewCSMethod(CSMethod csMethod) {
+        var assigns = assignMap.get(csMethod.getMethod());
+        if (assigns != null) {
+            Context ctx = csMethod.getContext();
+            assigns.forEach(pair -> {
+                Var lhs = pair.first();
+                Number number = pair.second();
+                Obj numberObj = heapModel.getMockObj(NUMBER_DESC, number, lhs.getType());
+                solver.addVarPointsTo(ctx, lhs, emptyCtx, numberObj);
+            });
+        }
+    }
+}
