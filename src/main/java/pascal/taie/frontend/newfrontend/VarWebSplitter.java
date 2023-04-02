f16f4b507ad90a07e8c4e839b03cd13bbd9f72c7
Bluesbreaker45 <bluesbreaker45@outlook.com>
2023-04-02 21:30:05 +0800

    Refactor for code readability.

--- src/main/java/pascal/taie/frontend/newfrontend/VarWebSplitter.java
+++ src/main/java/pascal/taie/frontend/newfrontend/VarWebSplitter.java
@@ -4,30 +4,9 @@ import org.objectweb.asm.tree.AbstractInsnNode;
 import pascal.taie.ir.exp.LValue;
 import pascal.taie.ir.exp.RValue;
 import pascal.taie.ir.exp.Var;
-import pascal.taie.ir.stmt.AssignLiteral;
-import pascal.taie.ir.stmt.Binary;
-import pascal.taie.ir.stmt.Cast;
-import pascal.taie.ir.stmt.Catch;
 import pascal.taie.ir.stmt.Copy;
 import pascal.taie.ir.stmt.DefinitionStmt;
-import pascal.taie.ir.stmt.Goto;
-import pascal.taie.ir.stmt.If;
-import pascal.taie.ir.stmt.InstanceOf;
-import pascal.taie.ir.stmt.Invoke;
-import pascal.taie.ir.stmt.LoadArray;
-import pascal.taie.ir.stmt.LoadField;
-import pascal.taie.ir.stmt.LookupSwitch;
-import pascal.taie.ir.stmt.Monitor;
-import pascal.taie.ir.stmt.New;
-import pascal.taie.ir.stmt.Nop;
-import pascal.taie.ir.stmt.Return;
 import pascal.taie.ir.stmt.Stmt;
-import pascal.taie.ir.stmt.StmtVisitor;
-import pascal.taie.ir.stmt.StoreArray;
-import pascal.taie.ir.stmt.StoreField;
-import pascal.taie.ir.stmt.TableSwitch;
-import pascal.taie.ir.stmt.Throw;
-import pascal.taie.ir.stmt.Unary;
 import pascal.taie.util.collection.Pair;
 import pascal.taie.util.collection.UnionFindSet;
 
@@ -48,14 +27,29 @@ public class VarWebSplitter {
 
     private final Map<AbstractInsnNode, List<Stmt>> auxiliaryStmts;
 
-    private final Map<
-            Var,
-            UnionFindSet<Pair<? extends Stmt, Kind>>
+    private final Map
+            <
+                    Var,
+                    UnionFindSet<Pair<? extends Stmt, Kind>>
             > webs;
 
-    private final Map<BytecodeBlock, Map<Var, Pair<Copy, Kind>>> inUse;
+    private final Map
+            <
+                    BytecodeBlock,
+                    Map<
+                            Var,
+                            Pair<Copy, Kind>
+                    >
+            > inUse;
 
-    private final Map<BytecodeBlock, Map<Var, Pair<? extends DefinitionStmt<? extends LValue, ? extends RValue>, Kind>>> outDef;
+    private final Map
+            <
+                    BytecodeBlock,
+                    Map<
+                            Var,
+                            Pair<? extends DefinitionStmt<? extends LValue, ? extends RValue>, Kind>
+                    >
+            > outDef;
 
     public VarWebSplitter(AsmIRBuilder builder) {
         this.builder = builder;
@@ -77,7 +71,7 @@ public class VarWebSplitter {
     /**
      *
      * @param var the variable you want to get the web information.
-     * @return the webs of the var. WARNING: in each set there may exist element whose Kind is PHANTOM.
+     * @return the webs of the variable. WARNING: in each set there may exist element whose Kind is PHANTOM.
      */
     public Collection<Set<Pair<? extends Stmt, Kind>>> getWebs(Var var) {
         return webs.get(var).getDisjointSets();
@@ -119,23 +113,21 @@ public class VarWebSplitter {
 
         Map<Var, Pair<? extends DefinitionStmt<? extends LValue, ? extends RValue>, Kind>> currentDefs = new HashMap<>(phantomUse);
 
-        DefVisitor defVisitor = new DefVisitor();
-        UseVisitor useVisitor = new UseVisitor();
         for (var stmt : getStmts(block)) {
             // uses first
-            List<Var> uses = stmt.accept(useVisitor);
+            List<Var> uses = stmt.getUses().stream().filter(r -> r instanceof Var).map(r -> (Var) r).toList();
             for (Var use : uses) {
                 var e = new Pair<>(stmt, Kind.USE);
-                var unionfind = webs.get(use);
-                unionfind.addElement(e);
-                unionfind.union(e, currentDefs.get(use));
+                var unionFind = webs.get(use);
+                unionFind.addElement(e);
+                unionFind.union(e, currentDefs.get(use));
             }
 
-            Var def = stmt.accept(defVisitor);
+            Var def = (Var) stmt.getDef().filter(l -> l instanceof Var).orElse(null);
             if (def != null) { // which means stmt is a DefinitionStmt.
                 var e = new Pair<>((DefinitionStmt<? extends LValue, ? extends RValue>) stmt, Kind.DEF);
-                var unionfind = webs.get(def);
-                unionfind.addElement(e);
+                var unionFind = webs.get(def);
+                unionFind.addElement(e);
                 var previous = currentDefs.put(def, e);
                 assert previous != null;
             }
@@ -164,229 +156,4 @@ public class VarWebSplitter {
         PHANTOM, // PHANTOM for the fake Copy in each block: x = x, used as a connector
     }
 
-    private static class DefVisitor implements StmtVisitor<Var> {
-
-        @Override
-        public Var visit(New stmt) {
-            return stmt.getLValue();
-        }
-
-        @Override
-        public Var visit(AssignLiteral stmt) {
-            return stmt.getLValue();
-        }
-
-        @Override
-        public Var visit(Copy stmt) {
-            return stmt.getLValue();
-        }
-
-        @Override
-        public Var visit(LoadArray stmt) {
-            return stmt.getLValue();
-        }
-
-        @Override
-        public Var visit(StoreArray stmt) {
-            return visitDefault(stmt);
-        }
-
-        @Override
-        public Var visit(LoadField stmt) {
-            return stmt.getLValue();
-        }
-
-        @Override
-        public Var visit(StoreField stmt) {
-            return visitDefault(stmt);
-        }
-
-        @Override
-        public Var visit(Binary stmt) {
-            return stmt.getLValue();
-        }
-
-        @Override
-        public Var visit(Unary stmt) {
-            return stmt.getLValue();
-        }
-
-        @Override
-        public Var visit(InstanceOf stmt) {
-            return stmt.getLValue();
-        }
-
-        @Override
-        public Var visit(Cast stmt) {
-            return stmt.getLValue();
-        }
-
-        @Override
-        public Var visit(Goto stmt) {
-            return visitDefault(stmt);
-        }
-
-        @Override
-        public Var visit(If stmt) {
-            return visitDefault(stmt);
-        }
-
-        @Override
-        public Var visit(TableSwitch stmt) {
-            return visitDefault(stmt);
-        }
-
-        @Override
-        public Var visit(LookupSwitch stmt) {
-            return visitDefault(stmt);
-        }
-
-        @Override
-        public Var visit(Invoke stmt) {
-            return stmt.getLValue();
-        }
-
-        @Override
-        public Var visit(Return stmt) {
-            return visitDefault(stmt);
-        }
-
-        @Override
-        public Var visit(Throw stmt) {
-            return visitDefault(stmt);
-        }
-
-        @Override
-        public Var visit(Catch stmt) {
-            return visitDefault(stmt);
-        }
-
-        @Override
-        public Var visit(Monitor stmt) {
-            return visitDefault(stmt);
-        }
-
-        @Override
-        public Var visit(Nop stmt) {
-            return visitDefault(stmt);
-        }
-
-        @Override
-        public Var visitDefault(Stmt stmt) {
-            return null;
-        }
-    }
-
-    private static class UseVisitor implements StmtVisitor<List<Var>> {
-
-        @Override
-        public List<Var> visit(New stmt) {
-            return visitDefault(stmt);
-        }
-
-        @Override
-        public List<Var> visit(AssignLiteral stmt) {
-            return visitDefault(stmt);
-        }
-
-        @Override
-        public List<Var> visit(Copy stmt) {
-            return visitDefault(stmt);
-        }
-
-        @Override
-        public List<Var> visit(LoadArray stmt) {
-            return visitDefault(stmt);
-        }
-
-        @Override
-        public List<Var> visit(StoreArray stmt) {
-            return visitDefault(stmt);
-        }
-
-        @Override
-        public List<Var> visit(LoadField stmt) {
-            return visitDefault(stmt);
-        }
-
-        @Override
-        public List<Var> visit(StoreField stmt) {
-            return visitDefault(stmt);
-        }
-
-        @Override
-        public List<Var> visit(Binary stmt) {
-            return visitDefault(stmt);
-        }
-
-        @Override
-        public List<Var> visit(Unary stmt) {
-            return visitDefault(stmt);
-        }
-
-        @Override
-        public List<Var> visit(InstanceOf stmt) {
-            return visitDefault(stmt);
-        }
-
-        @Override
-        public List<Var> visit(Cast stmt) {
-            return visitDefault(stmt);
-        }
-
-        @Override
-        public List<Var> visit(Goto stmt) {
-            return visitDefault(stmt);
-        }
-
-        @Override
-        public List<Var> visit(If stmt) {
-            return visitDefault(stmt);
-        }
-
-        @Override
-        public List<Var> visit(TableSwitch stmt) {
-            return visitDefault(stmt);
-        }
-
-        @Override
-        public List<Var> visit(LookupSwitch stmt) {
-            return visitDefault(stmt);
-        }
-
-        @Override
-        public List<Var> visit(Invoke stmt) {
-            return visitDefault(stmt);
-        }
-
-        @Override
-        public List<Var> visit(Return stmt) {
-            return visitDefault(stmt);
-        }
-
-        @Override
-        public List<Var> visit(Throw stmt) {
-            return visitDefault(stmt);
-        }
-
-        @Override
-        public List<Var> visit(Catch stmt) {
-            return visitDefault(stmt);
-        }
-
-        @Override
-        public List<Var> visit(Monitor stmt) {
-            return visitDefault(stmt);
-        }
-
-        @Override
-        public List<Var> visit(Nop stmt) {
-            return visitDefault(stmt);
-        }
-
-        @Override
-        public List<Var> visitDefault(Stmt stmt) {
-            return new ArrayList<>();
-        }
-    }
 }
