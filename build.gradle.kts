feee7f282ee1759baaeef6a724eefd1078f3ad6b
Teng Zhang <zhangt2333@gmail.com>
2023-04-28 20:30:19 +0800

    Upgrade dependencies and disable transitive dependencies from Soot in the compile classpath

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,17 +5,23 @@ plugins {
 
 dependencies {
     // Process options
-    implementation("info.picocli:picocli:4.7.1")
+    implementation("info.picocli:picocli:4.7.3")
     // Logger
-    implementation("org.apache.logging.log4j:log4j-api:2.19.0")
-    implementation("org.apache.logging.log4j:log4j-core:2.19.0")
+    implementation("org.apache.logging.log4j:log4j-api:2.20.0")
+    implementation("org.apache.logging.log4j:log4j-core:2.20.0")
     // Process YAML configuration files
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.14.2")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.15.0")
     // Use Soot as frontend
     implementation(files("lib/sootclasses-modified.jar"))
-    implementation("org.soot-oss:soot:4.4.1")
+    "org.soot-oss:soot:4.4.1".let {
+        // Disable transitive dependencies from Soot in compile classpath
+        implementation(it) { isTransitive = false }
+        runtimeOnly(it)
+    }
     // Eliminate SLF4J warning
-    implementation("org.slf4j:slf4j-nop:2.0.6")
+    implementation("org.slf4j:slf4j-nop:2.0.7")
+    // JSR305, for javax.annotation
+    implementation("com.google.code.findbugs:jsr305:3.0.2")
 
     testImplementation("junit:junit:4.13.2")
 }
