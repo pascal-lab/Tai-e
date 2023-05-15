9dadeae0f191a3aa1242caf78a81130a4e8b9baa
Bluesbreaker45 <bluesbreaker45@outlook.com>
2023-05-15 20:58:31 +0800

    Partial implementation.

--- src/main/java/pascal/taie/project/OptionsProjectBuilder.java
+++ src/main/java/pascal/taie/project/OptionsProjectBuilder.java
@@ -6,10 +6,13 @@ import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class OptionsProjectBuilder extends AbstractProjectBuilder {
 
     private final Options options;
+
+    private Project project;
 
     public OptionsProjectBuilder(Options options) {
         this.options = options;
@@ -17,35 +20,40 @@ public class OptionsProjectBuilder extends AbstractProjectBuilder {
 
     @Override
     protected String getMainClass() {
-        return null;
+        return options.getMainClass();
     }
 
     @Override
     protected int getJavaVersion() {
-        return 0;
+        return options.getJavaVersion();
     }
 
     @Override
     protected List<String> getInputClasses() {
-        return null;
+        return options.getInputClasses();
     }
 
     @Override
     protected List<FileContainer> getRootContainers() {
-        return null;
+        return Stream.concat(
+                project.getAppRootContainers().stream(),
+                project.getLibRootContainers().stream()
+        ).toList();
     }
 
     @Override
     public Project build() {
         try {
-            return new Project(options.getMainClass(),
+            project = new Project(
+                    options.getMainClass(),
                     options.getJavaVersion(),
                     options.getInputClasses(),
                     FileLoader.get().loadRootContainers(
-                            Arrays.stream(options.getClassPath().split(";")).map(Paths::get).toList()),  // TODO: change to options.getAppClassPath() after modifying Options
+                            Arrays.stream(options.getAppClassPath().split(";")).map(Paths::get).toList()),  // TODO: change to options.getAppClassPath() after modifying Options
                     FileLoader.get().loadRootContainers(
                             Arrays.stream(new String[]{}).map(Paths::get).toList()) // TODO: change to options.getLibClassPath() after modifying Options
             );
+            return project;
         } catch (IOException e) {
             // TODO: more info
             e.printStackTrace();
