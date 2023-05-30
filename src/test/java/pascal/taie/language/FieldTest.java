38a8caa25627421a35fc3fd69a90e0b4053bb2af
Teng Zhang <zhangt2333@gmail.com>
2023-05-30 12:08:00 +0800

    Add test case for same-named fields

---
+++ src/test/java/pascal/taie/language/FieldTest.java
@@ -0,0 +1,62 @@
+/*
+ * Tai-e: A Static Analysis Framework for Java
+ *
+ * Copyright (C) 2022 Tian Tan <tiantan@nju.edu.cn>
+ * Copyright (C) 2022 Yue Li <yueli@nju.edu.cn>
+ *
+ * This file is part of Tai-e.
+ *
+ * Tai-e is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Tai-e is distributed in the hope that it will be useful,but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Tai-e. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+package pascal.taie.language;
+
+import org.junit.Assert;
+import org.junit.Test;
+import pascal.taie.Main;
+import pascal.taie.World;
+import pascal.taie.language.classes.ClassHierarchy;
+import pascal.taie.language.classes.JField;
+
+public class FieldTest {
+
+    @Test
+    public void testFieldsWithSameName() {
+        String clz = "FieldsWithSameName";
+        Main.buildWorld("-cp", "src/test/resources/world",
+                "--main-class", clz);
+        ClassHierarchy hierarchy = World.get().getClassHierarchy();
+        JField sf1 = hierarchy.getField("<FieldsWithSameName: java.lang.String a>");
+        JField sf2 = hierarchy.getField("<FieldsWithSameName: java.lang.Character a>");
+        JField if1 = hierarchy.getField("<FieldsWithSameName: java.lang.String b>");
+        JField if2 = hierarchy.getField("<FieldsWithSameName: java.lang.Character b>");
+        Assert.assertEquals(clz, sf1.getDeclaringClass().getName());
+        Assert.assertEquals(clz, sf2.getDeclaringClass().getName());
+        Assert.assertEquals(clz, if1.getDeclaringClass().getName());
+        Assert.assertEquals(clz, if2.getDeclaringClass().getName());
+        Assert.assertEquals("a", sf1.getName());
+        Assert.assertEquals("a", sf2.getName());
+        Assert.assertEquals("b", if1.getName());
+        Assert.assertEquals("b", if2.getName());
+        Assert.assertTrue(sf1.isStatic());
+        Assert.assertTrue(sf2.isStatic());
+        Assert.assertFalse(if1.isStatic());
+        Assert.assertFalse(if2.isStatic());
+        Assert.assertEquals("java.lang.String", sf1.getType().getName());
+        Assert.assertEquals("java.lang.Character", sf2.getType().getName());
+        Assert.assertEquals("java.lang.String", if1.getType().getName());
+        Assert.assertEquals("java.lang.Character", if2.getType().getName());
+    }
+}
