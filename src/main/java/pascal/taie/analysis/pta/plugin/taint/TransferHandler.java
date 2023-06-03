bf9012bfe7ac979a7592f630aac9714a0fdde746
Tian Tan <silverbullettt@gmail.com>
2023-06-03 22:58:36 +0800

    TransferHandler supports call-site mode

--- src/main/java/pascal/taie/analysis/pta/plugin/taint/TransferHandler.java
+++ src/main/java/pascal/taie/analysis/pta/plugin/taint/TransferHandler.java
@@ -56,6 +56,7 @@ import pascal.taie.util.collection.MultiMap;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Handles taint transfers in taint analysis.
@@ -86,6 +87,13 @@ class TransferHandler extends OnFlyHandler {
     private final MultiMap<Var, TransferInfo> transferInfos = Maps.newMultiMap();
 
     /**
+     * Map from a method to {@link Invoke} statements in the method
+     * which matches any transfer method.
+     * This map matters only when call-site mode is enabled.
+     */
+    private final MultiMap<JMethod, Invoke> callSiteTransfers = Maps.newMultiMap();
+
+    /**
      * Whether enable taint back propagation to handle aliases about
      * tainted mutable objects, e.g., char[].
      */
@@ -109,31 +117,16 @@ class TransferHandler extends OnFlyHandler {
                 .forEach(t -> this.transfers.put(t.method(), t));
     }
 
-    @Override
-    public void onNewCallEdge(Edge<CSCallSite, CSMethod> edge) {
-        if (edge.getKind() == CallKind.OTHER) {
-            // skip other call edges, e.g., reflective call edges,
-            // which currently cannot be handled for transfer methods
-            // TODO: handle OTHER call edges
-            return;
-        }
-        JMethod callee = edge.getCallee().getMethod();
-        transfers.get(callee).forEach(transfer ->
-                processTransfer(edge.getCallSite(), transfer));
-    }
-
-    private void processTransfer(CSCallSite callSite, TaintTransfer transfer) {
-        Invoke invoke = callSite.getCallSite();
+    private void processTransfer(Context context, Invoke callSite, TaintTransfer transfer) {
         TransferPoint from = transfer.from();
         TransferPoint to = transfer.to();
-        Var toVar = InvokeUtils.getVar(invoke, to.index());
+        Var toVar = InvokeUtils.getVar(callSite, to.index());
         if (toVar == null) {
             return;
         }
-        Context ctx = callSite.getContext();
-        Var fromVar = InvokeUtils.getVar(invoke, from.index());
-        CSVar csFrom = csManager.getCSVar(ctx, fromVar);
-        CSVar csTo = csManager.getCSVar(ctx, toVar);
+        Var fromVar = InvokeUtils.getVar(callSite, from.index());
+        CSVar csFrom = csManager.getCSVar(context, fromVar);
+        CSVar csTo = csManager.getCSVar(context, toVar);
         if (from.kind() == TransferPoint.Kind.VAR) { // Var -> Var/Array/Field
             Kind kind = switch (to.kind()) {
                 case VAR -> {
@@ -147,7 +140,7 @@ class TransferHandler extends OnFlyHandler {
             if (kind != null) {
                 TransferInfo info = new TransferInfo(kind, fromVar, transfer);
                 transferInfos.put(toVar, info);
-                transferTaint(solver.getPointsToSetOf(csTo), ctx, info);
+                transferTaint(solver.getPointsToSetOf(csTo), context, info);
             }
         } else if (to.kind() == TransferPoint.Kind.VAR) { // Array/Field -> Var
             Kind kind = switch (from.kind()) {
@@ -157,7 +150,7 @@ class TransferHandler extends OnFlyHandler {
             };
             TransferInfo info = new TransferInfo(kind, toVar, transfer);
             transferInfos.put(fromVar, info);
-            transferTaint(solver.getPointsToSetOf(csFrom), ctx, info);
+            transferTaint(solver.getPointsToSetOf(csFrom), context, info);
         } else { // ignore other cases
             logger.warn("TaintTransfer {} -> {} (in {}) is not supported",
                     transfer, from.kind(), to.kind());
@@ -175,7 +168,7 @@ class TransferHandler extends OnFlyHandler {
                 && to.kind() == TransferPoint.Kind.VAR
                 && !(to.index() == InvokeUtils.BASE
                 && transfer.method().isConstructor())) {
-            backPropagateTaint(toVar, ctx);
+            backPropagateTaint(toVar, context);
         }
     }
 
@@ -227,13 +220,6 @@ class TransferHandler extends OnFlyHandler {
                 }));
     }
 
-    @Override
-    public void onNewPointsToSet(CSVar csVar, PointsToSet pts) {
-        Context ctx = csVar.getContext();
-        transferInfos.get(csVar.getVar()).forEach(info ->
-                transferTaint(pts, ctx, info));
-    }
-
     private void backPropagateTaint(Var to, Context ctx) {
         CSMethod csMethod = csManager.getCSMethod(ctx, to.getMethod());
         solver.addStmts(csMethod,
@@ -281,5 +267,56 @@ class TransferHandler extends OnFlyHandler {
     private Var getTempVar(JMethod container, Type type) {
         String varName = "%taint-temp-" + counter++;
         return new Var(container, varName, type, -1);
+    }
+
+    @Override
+    public void onNewCallEdge(Edge<CSCallSite, CSMethod> edge) {
+        if (edge.getKind() == CallKind.OTHER) {
+            // skip other call edges, e.g., reflective call edges,
+            // which currently cannot be handled for transfer methods
+            // TODO: handle OTHER call edges
+            return;
+        }
+        Set<TaintTransfer> tfs = transfers.get(edge.getCallee().getMethod());
+        if (!tfs.isEmpty()) {
+            Context context = edge.getCallSite().getContext();
+            Invoke callSite = edge.getCallSite().getCallSite();
+            tfs.forEach(tf -> processTransfer(context, callSite, tf));
+        }
+    }
+
+    @Override
+    public void onNewPointsToSet(CSVar csVar, PointsToSet pts) {
+        Context ctx = csVar.getContext();
+        transferInfos.get(csVar.getVar()).forEach(info ->
+                transferTaint(pts, ctx, info));
+    }
+
+    @Override
+    public void onNewMethod(JMethod method) {
+        if (callSiteMode) {
+            method.getIR().invokes(false).forEach(callSite -> {
+                JMethod callee = callSite.getMethodRef().resolveNullable();
+                if (transfers.containsKey(callee)) {
+                    callSiteTransfers.put(method, callSite);
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onNewCSMethod(CSMethod csMethod) {
+        if (callSiteMode) {
+            JMethod method = csMethod.getMethod();
+            Set<Invoke> callSites = callSiteTransfers.get(method);
+            if (!callSites.isEmpty()) {
+                Context context = csMethod.getContext();
+                callSites.forEach(callSite -> {
+                    JMethod callee = callSite.getMethodRef().resolve();
+                    transfers.get(callee).forEach(transfer ->
+                            processTransfer(context, callSite, transfer));
+                });
+            }
+        }
     }
 }
