910472090ebfdfe85e65fae1f9a29e01fcc6cb62
Chenxi Li <ayanamists@gmail.com>
2023-07-13 13:26:27 +0800

    Improve DepCWBuilder   - use handwrite constant parser   - build in parallel

---
+++ src/main/java/pascal/taie/frontend/newfrontend/ConstantTableReader.java
@@ -0,0 +1,156 @@
+package pascal.taie.frontend.newfrontend;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.objectweb.asm.Type;
+import pascal.taie.util.collection.Sets;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class ConstantTableReader {
+    public static final int HEAD = 0xcafebabe;
+    // Constant pool types
+    public static final byte CONSTANT_Utf8 = 1;
+    public static final byte CONSTANT_Integer = 3;
+    public static final byte CONSTANT_Float = 4;
+    public static final byte CONSTANT_Long = 5;
+    public static final byte CONSTANT_Double = 6;
+    public static final byte CONSTANT_Class = 7;
+    public static final byte CONSTANT_String = 8;
+    public static final byte CONSTANT_FieldRef = 9;
+    public static final byte CONSTANT_MethodRef = 10;
+    public static final byte CONSTANT_InterfaceMethodRef = 11;
+    public static final byte CONSTANT_NameAndType = 12;
+    public static final byte CONSTANT_MethodHandle = 15;
+    public static final byte CONSTANT_MethodType = 16;
+    public static final byte CONSTANT_InvokeDynamic = 18;
+    public static final byte CONSTANT_Module = 19;
+    public static final byte CONSTANT_Package = 20;
+
+    private static final Logger logger = LogManager.getLogger();
+
+    private List<String> binaryNames;
+
+    public ConstantTableReader() {
+    }
+
+    public List<String> read(byte[] content) {
+        parse(ByteBuffer.wrap(content));
+        return binaryNames;
+    }
+
+    void parse(ByteBuffer buf) {
+        binaryNames = new ArrayList<>();
+        if (buf.order(ByteOrder.BIG_ENDIAN).getInt() != HEAD) {
+            throw new FrontendException("not a valid class file");
+        }
+
+        int minor = buf.getChar(), ver = buf.getChar();
+        int count = buf.getChar();
+        String[] constants = new String[count];
+        Set<Integer> internals = Sets.newSet(count / 2);
+        Set<Integer> descriptors = Sets.newSet(count / 2);
+        for (int ix = 1; ix < count; ix++) {
+            int index1 = -1, index2 = -1;
+            byte tag = buf.get();
+            switch (tag) {
+                default -> throw new FrontendException("unknown pool item type " + buf.get(buf.position() - 1));
+                case CONSTANT_Utf8 -> {
+                    String str = decodeString(buf);
+                    constants[ix] = str;
+                }
+                case CONSTANT_Class -> {
+                    index1 = buf.getChar();
+                    internals.add(index1);
+                }
+                case CONSTANT_MethodType -> {
+                    index1 = buf.getChar();
+                    descriptors.add(index1);
+                }
+                case CONSTANT_FieldRef, CONSTANT_MethodRef, CONSTANT_InterfaceMethodRef -> {
+                    index1 = buf.getChar();
+                    buf.getChar();
+                    internals.add(index1);
+                }
+                case CONSTANT_NameAndType -> {
+                    buf.getChar();
+                    index2 = buf.getChar();
+                    descriptors.add(index2);
+                }
+                case CONSTANT_Double, CONSTANT_Long -> {
+                    buf.getLong();
+                    ix++;
+                }
+                case CONSTANT_MethodHandle -> {
+                    buf.get();
+                    buf.getChar();
+                }
+                case CONSTANT_InvokeDynamic -> {
+                    buf.getChar();
+                    buf.getChar();
+                }
+                case CONSTANT_Integer -> buf.getInt();
+                case CONSTANT_Float -> buf.getFloat();
+                case CONSTANT_Module, CONSTANT_Package, CONSTANT_String -> buf.getChar();
+            }
+        }
+
+        internals.forEach(i -> addInternalName(constants[i]));
+        descriptors.forEach(i -> addDescriptor(constants[i]));
+    }
+
+    private void addBinaryName(String binaryName) {
+        binaryNames.add(binaryName);
+    }
+
+    private void addDescriptor(String descriptor) {
+        addType(Type.getType(descriptor));
+    }
+
+    private void addInternalName(String internalName) {
+        if (internalName == null) {
+            return;
+        }
+        addType(Type.getObjectType(internalName));
+    }
+
+    private void addType(Type t) {
+        if (t.getSort() == Type.ARRAY) {
+            addType(t.getElementType());
+        } else if (t.getSort() == Type.OBJECT) {
+            addBinaryName(t.getClassName());
+        } else if (t.getSort() == Type.METHOD) {
+            for (var i : t.getArgumentTypes()) {
+                addType(i);
+            }
+            addType(t.getReturnType());
+        }
+    }
+
+    // TODO: optimize
+    private static String decodeString(ByteBuffer buf) {
+        int size = buf.getChar(), oldLimit = buf.limit();
+        buf.limit(buf.position() + size);
+        StringBuilder sb = new StringBuilder(size + (size >> 1));
+        while (buf.hasRemaining()) {
+            byte b = buf.get();
+            if (b > 0)
+                sb.append((char) b);
+            else {
+                int b2 = buf.get();
+                if ((b & 0xf0) != 0xe0)
+                    sb.append((char) ((b & 0x1F) << 6 | b2 & 0x3F));
+                else {
+                    int b3 = buf.get();
+                    sb.append((char) ((b & 0x0F) << 12 | (b2 & 0x3F) << 6 | b3 & 0x3F));
+                }
+            }
+        }
+        buf.limit(oldLimit);
+        return sb.toString();
+    }
+}
--- src/main/java/pascal/taie/frontend/newfrontend/DepCWBuilder.java
+++ src/main/java/pascal/taie/frontend/newfrontend/DepCWBuilder.java
@@ -8,25 +8,43 @@ import pascal.taie.project.ClassFile;
 import pascal.taie.project.JavaSourceFile;
 import pascal.taie.project.Project;
 import pascal.taie.util.collection.Maps;
+import pascal.taie.util.collection.Sets;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Queue;
+import java.util.Set;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class DepCWBuilder implements ClosedWorldBuilder {
 
     private static final Logger logger = LogManager.getLogger(DepCWBuilder.class);
 
-    private final Map<String, ClassSource> sourceMap;
+    private static final int THREAD_POOL_SIZE = 8;
+
+    private final ConcurrentMap<String, ClassSource> sourceMap;
+
+    private final Set<String> founded;
+
+    private final CompletionService<Completed> completionService;
 
     private Project project;
 
     public DepCWBuilder() {
-        sourceMap = Maps.newHybridMap();
+        sourceMap = Maps.newConcurrentMap();
+        founded = Sets.newHybridSet();
+        Executor executor = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+        completionService = new ExecutorCompletionService<>(executor);
     }
 
     @Override
@@ -43,54 +61,87 @@ public class DepCWBuilder implements ClosedWorldBuilder {
     public void build(Project p) {
         String entry = p.getMainClass();
         this.project = p;
+        List<String> target = new ArrayList<>();
         try {
             if (entry != null) {
-                buildClosure(entry);
+                target.add(entry);
             }
-            for (var i : p.getInputClasses()) {
-                buildClosure(i);
-            }
+            target.addAll(p.getInputClasses());
+            buildClosure(target);
         } catch (IOException e) {
             // TODO: fail info
-            throw new RuntimeException(e);
+            throw new FrontendException(e.getMessage());
         }
     }
 
-    private void buildClosure(String binaryName) throws IOException {
-        if (binaryName.equals("android$widget$RemoteViews$BaseReflectionAction")) {
+    private void buildClosure(List<String> binaryNames) throws IOException {
+        String excluded = "android$widget$RemoteViews$BaseReflectionAction";
+        if (binaryNames.contains(excluded)) {
             // FIX ME: a workaround to skip android test case in test/resources/android$widget$RemoteViews$BaseReflectionAction.class
+            binaryNames.remove(excluded);
             return;
         }
-        Queue<String> workList = new LinkedList<>();
-        workList.add(binaryName);
-        while (! workList.isEmpty()) {
-            binaryName = workList.poll();
-            if (sourceMap.containsKey(binaryName)) {
-                continue;
-            }
 
-            AnalysisFile f = project.locate(binaryName);
-            if (f == null) {
-                throw new FileNotFoundException(binaryName);
-            }
+        Queue<String> workList = new LinkedList<>(binaryNames);
 
-            if (f instanceof JavaSourceFile) {
-                logger.warn(
-                        "WARNING: currently new frontend does not support java source code("
-                        + ((JavaSourceFile) f).getClassName()
-                        + "). So this class would be ignored and the rest task continues on.");
-                return;
-            }
+        // deltaCount means (founded, not completed) - completed, a.k.a "on the fly"
+        // it MUST be equal to the task that
+        // completionService are current running (not retried by user)
+        int deltaCount = 0;
 
-            List<String> deps = null;
-            if (f instanceof JavaSourceFile jFile) {
-                // TODO: fill here
-            } else if (f instanceof ClassFile cFile) {
-                deps = buildClassDeps(binaryName, cFile);
-            } else {
-                throw new IllegalStateException();
+        // this loop will satisfy:
+        // deltaCount_{before} < deltaCount_{after} \/ tempDeltaCount == 0
+        while (! workList.isEmpty() || deltaCount > 0) {
+
+            // first, poll all classes from workList
+            // They will be built in parallel
+            int tempDeltaCount = 0;
+            while (! workList.isEmpty()) {
+                String binaryName = workList.poll();
+                if (!founded.contains(binaryName)) {
+                    founded.add(binaryName);
+                    completionService.submit(() ->
+                            new Completed(binaryName, buildDeps(binaryName)));
+                    tempDeltaCount ++;
+                }
             }
-            workList.addAll(deps);
+            deltaCount += tempDeltaCount;
+
+            // if there are enough classes to wait for,
+            // or this iteration do not found new class
+            //    (which means you have to process all remaining classes)
+            // wait them for build completed
+            if (deltaCount >= THREAD_POOL_SIZE || tempDeltaCount == 0) {
+                while (deltaCount > 0) {
+                    try {
+                        Future<Completed> future = completionService.take();
+                        Completed res = future.get();
+                        workList.addAll(res.res);
+                        deltaCount--;
+                    } catch (InterruptedException | ExecutionException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+    }
+
+    private List<String> buildDeps(String binaryName) throws IOException {
+        AnalysisFile f = project.locate(binaryName);
+        if (f == null) {
+            throw new FileNotFoundException(binaryName);
+        }
+
+        if (f instanceof JavaSourceFile) {
+            logger.warn(
+                    "WARNING: currently new frontend does not support java source code("
+                    + ((JavaSourceFile) f).getClassName()
+                    + "). So this class would be ignored and the rest task continues on.");
+            return List.of();
+        } else if (f instanceof ClassFile classFile) {
+            return buildClassDeps(binaryName, classFile);
+        } else {
+            throw new UnsupportedOperationException();
         }
     }
 
@@ -101,9 +152,9 @@ public class DepCWBuilder implements ClosedWorldBuilder {
         byte[] content = cFile.resource().getContent();
         ClassReader reader = new ClassReader(content);
         assert reader.getClassName().replaceAll("/", ".").equals(binaryName);
-        DepClassVisitor v = new DepClassVisitor();
         sourceMap.put(binaryName, new AsmSource(reader, isApplication));
-        reader.accept(v, ClassReader.SKIP_FRAMES);
-        return v.getBinaryNames().stream().toList();
+        return new ConstantTableReader().read(content);
     }
+
+    private record Completed(String input, List<String> res) {}
 }
