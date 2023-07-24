cfd12de2bfe2001b3ba61b9e18ff0d879080ff42
Yu Wu <yuwu@smail.nju.edu.cn>
2023-07-24 11:20:03 +0800

    Update TypeReachability

--- src/main/java/pascal/taie/analysis/pta/plugin/taint/TypeReachability.java
+++ src/main/java/pascal/taie/analysis/pta/plugin/taint/TypeReachability.java
@@ -2,8 +2,9 @@ package pascal.taie.analysis.pta.plugin.taint;
 
 import pascal.taie.language.type.Type;
 import pascal.taie.util.collection.Sets;
-import pascal.taie.util.graph.Reachability;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Set;
 
 class TypeReachability {
@@ -19,31 +20,45 @@ class TypeReachability {
     }
 
     // Return new types that can reach sink
-    public Set<Type> addSinkTypes(Set<Type> newSinkTypes) {
-        Set<Type> newTypesCanReachSink = Sets.newSet();
-        Reachability<Type> reachability = new Reachability<>(typeGraph);
-        for (Type type : newSinkTypes) {
-            if (sinkTypes.add(type)) {
-                newTypesCanReachSink.addAll(reachability.nodesCanReach(type));
-            }
+    public Set<Type> addSinkType(Type newSinkType) {
+        if (sinkTypes.add(newSinkType)) {
+            Set<Type> newTypesCanReachSink = getNewTypesCanReachSink(newSinkType);
+            typesCanReachSink.addAll(newTypesCanReachSink);
+            return newTypesCanReachSink;
+        } else {
+            return Set.of();
         }
-        newTypesCanReachSink.removeAll(typesCanReachSink);
-        typesCanReachSink.addAll(newTypesCanReachSink);
-        return newTypesCanReachSink;
     }
 
     public Set<Type> addTypeTransfer(Type from, Type to) {
-        if(!typeGraph.hasEdge(from, to)) {
+        if (!typeGraph.hasEdge(from, to)) {
             typeGraph.addEdge(from, to);
-            Set<Type> newTypesCanReachSink = Sets.newSet();
-            if(!typesCanReachSink.contains(from) && typesCanReachSink.contains(to)) {
-                newTypesCanReachSink.addAll(new Reachability<>(typeGraph).nodesCanReach(from));
+            if (!typesCanReachSink.contains(from) && typesCanReachSink.contains(to)) {
+                Set<Type> newTypesCanReachSink = getNewTypesCanReachSink(from);
+                typesCanReachSink.addAll(newTypesCanReachSink);
+                return newTypesCanReachSink;
+            } else {
+                return Set.of();
             }
-            newTypesCanReachSink.removeAll(typesCanReachSink);
-            typesCanReachSink.addAll(newTypesCanReachSink);
-            return newTypesCanReachSink;
         }
         return Set.of();
+    }
+
+    // Only need to process predecessors of 'from' which currently cannot reach sink type
+    private Set<Type> getNewTypesCanReachSink(Type from) {
+        Set<Type> visited = Sets.newSet();
+        Deque<Type> stack = new ArrayDeque<>();
+        stack.push(from);
+        while (!stack.isEmpty()) {
+            Type node = stack.pop();
+            if (visited.add(node)) {
+                typeGraph.getSuccsOf(node)
+                        .stream()
+                        .filter(type -> !visited.contains(type) && !typesCanReachSink.contains(type))
+                        .forEach(stack::push);
+            }
+        }
+        return visited;
     }
 
     public boolean canReachSink(Type type) {
