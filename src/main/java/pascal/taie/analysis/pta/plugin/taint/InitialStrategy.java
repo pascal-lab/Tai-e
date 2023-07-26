a4e8b35bd90a41eb092a9cfb16c110e706ad13e6
Yu Wu <yuwu@smail.nju.edu.cn>
2023-07-26 16:41:06 +0800

    Update InitialStrategy

--- src/main/java/pascal/taie/analysis/pta/plugin/taint/InitialStrategy.java
+++ src/main/java/pascal/taie/analysis/pta/plugin/taint/InitialStrategy.java
@@ -4,21 +4,23 @@ import pascal.taie.analysis.graph.callgraph.CallGraph;
 import pascal.taie.analysis.pta.core.cs.element.CSCallSite;
 import pascal.taie.analysis.pta.core.cs.element.CSManager;
 import pascal.taie.analysis.pta.core.cs.element.CSMethod;
-import pascal.taie.analysis.pta.core.cs.element.CSObj;
-import pascal.taie.analysis.pta.core.heap.Obj;
+import pascal.taie.analysis.pta.core.cs.element.CSVar;
 import pascal.taie.analysis.pta.core.solver.Solver;
 import pascal.taie.analysis.pta.plugin.util.InvokeUtils;
-import pascal.taie.analysis.pta.pts.PointsToSet;
+import pascal.taie.analysis.pta.plugin.util.StrategyUtils;
 import pascal.taie.language.classes.JClass;
 import pascal.taie.language.classes.JMethod;
+import pascal.taie.language.type.Type;
 import pascal.taie.util.collection.Maps;
 import pascal.taie.util.collection.MultiMap;
+import pascal.taie.util.collection.MultiMapCollector;
 import pascal.taie.util.collection.Sets;
+import pascal.taie.util.collection.TwoKeyMap;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
 class InitialStrategy implements TransInferStrategy {
@@ -26,79 +28,76 @@ class InitialStrategy implements TransInferStrategy {
     private static final int BASE = InvokeUtils.BASE;
 
     private static final int RESULT = InvokeUtils.RESULT;
-    private final MultiMap<JMethod, CSCallSite> method2CSCallSite = Maps.newMultiMap();
+    private final TwoKeyMap<JMethod, Integer, Set<Type>> arg2types = Maps.newTwoKeyMap();
     private Set<JMethod> methodsWithTransfer;
     private Set<JClass> ignoreClasses;
     private Set<JMethod> ignoreMethods;
-    private CSManager csManager;
-    private Solver solver;
 
     @Override
     public void setContext(InfererContext context) {
-        this.solver = context.solver();
+        Solver solver = context.solver();
         TaintConfig taintConfig = context.config();
+        CSManager csManager = solver.getCSManager();
         this.methodsWithTransfer = taintConfig.transfers().stream()
                 .map(TaintTransfer::getMethod)
                 .collect(Collectors.toSet());
         this.ignoreClasses = Sets.newSet(taintConfig.inferenceConfig().ignoreClasses());
         this.ignoreMethods = Sets.newSet(taintConfig.inferenceConfig().ignoreMethods());
-        this.csManager = solver.getCSManager();
 
         CallGraph<CSCallSite, CSMethod> callGraph = solver.getCallGraph();
+        MultiMap<JMethod, CSCallSite> method2CSCallSite = Maps.newMultiMap();
         callGraph.reachableMethods()
                 .forEach(csMethod ->
                         method2CSCallSite.putAll(csMethod.getMethod(), callGraph.getCallersOf(csMethod)));
+
+        for(JMethod method : method2CSCallSite.keySet()) {
+            // TODO: fix undefined behavior
+            for(int i = RESULT; i < method.getParamCount(); i++) {
+                int finalI = i;
+                Set<Type> types= method2CSCallSite.get(method).stream()
+                        .map(csCallSite -> {
+                            CSVar csVar = StrategyUtils.getCSVar(csManager, csCallSite, finalI);
+                            return StrategyUtils.getTypes(solver, csVar);
+                        })
+                        .flatMap(Collection::stream)
+                        .collect(Collectors.toSet());
+                arg2types.put(method, i, types);
+            }
+        }
     }
 
     @Override
     public Set<TaintTransfer> apply(JMethod method, Set<TaintTransfer> transfers) {
-        // check whether this method or its class needs to be ignored
-        if (ignoreMethods.contains(method) || ignoreClasses.contains(method.getDeclaringClass()))
-            return Set.of();
-        // check if exists transfer for this method
-        if (methodsWithTransfer.contains(method))
+        if (ignoreMethods.contains(method)
+                || ignoreClasses.contains(method.getDeclaringClass())
+                || methodsWithTransfer.contains(method))
             return Set.of();
 
         // add whole transfers for this method
-        Set<TaintTransfer> taintTransferSet = Sets.newSet();
-        Set<CSCallSite> csCallSites = method2CSCallSite.get(method);
+        Set<TaintTransfer> result = Sets.newSet();
         if (!method.isStatic()) {
-            // add base-to-result transfer
-            TransferPoint fromPoint = new TransferPoint(TransferPoint.Kind.VAR, BASE, null);
-            TransferPoint toPoint = new TransferPoint(TransferPoint.Kind.VAR, RESULT, null);
-            addInferredTransfers(method, taintTransferSet, fromPoint, toPoint, csCallSites, RESULT);
-            // add arg-to-base transfer(s)
-            addInferredTransfersFromArgs(method, taintTransferSet, fromPoint, toPoint, csCallSites, BASE);
+            // base-to-result
+            addTransfers(result, method, BASE, RESULT);
+            // arg-to-base
+            for (int i = 0; i < method.getParamCount(); i++) {
+                addTransfers(result, method, i, BASE);
+            }
         }
-        // add arg-to-result transfer(s)
-        addInferredTransfersFromArgs(method, taintTransferSet, fromPoint, toPoint, csCallSites, RESULT);
-
-        return Collections.unmodifiableSet(taintTransferSet);
-    }
-
-    private void addInferredTransfersFromArgs(JMethod method, Set<TaintTransfer> taintTransferSet,
-                                              AtomicReference<TransferPoint> fromPoint,
-                                              AtomicReference<TransferPoint> toPoint,
-                                              Set<CSCallSite> csCallSites, int target) {
+        // arg-to-result
         for (int i = 0; i < method.getParamCount(); i++) {
-            fromPoint.set(new TransferPoint(TransferPoint.Kind.VAR, i, null));
-            toPoint.set(new TransferPoint(TransferPoint.Kind.VAR, target, null));
-            addInferredTransfers(method, taintTransferSet, fromPoint, toPoint, csCallSites, target);
+            addTransfers(result, method, i, RESULT);
         }
+
+        return Collections.unmodifiableSet(result);
     }
 
-    private void addInferredTransfers(JMethod method, Set<TaintTransfer> taintTransferSet,
-                                      AtomicReference<TransferPoint> fromPoint,
-                                      AtomicReference<TransferPoint> toPoint,
-                                      Set<CSCallSite> csCallSites, int target) {
-        csCallSites.stream().map(csCallSite -> getCSVar(csCallSite, target))
-                .map(solver::getPointsToSetOf)
-                .map(PointsToSet::getObjects)
-                .flatMap(Collection::stream)
-                .map(CSObj::getObject)
-                .map(Obj::getType)
-                .forEach(type -> taintTransferSet
-                        .add(new InferredTransfer(method, fromPoint.get(), toPoint.get(), type, getWeight())));
+    private void addTransfers(Set<TaintTransfer> result, JMethod method, int from, int to) {
+        Set<Type> toTypes = arg2types.getOrDefault(method, to, Set.of());
+        TransferPoint fromPoint = new TransferPoint(TransferPoint.Kind.VAR, from, null);
+        TransferPoint toPoint = new TransferPoint(TransferPoint.Kind.VAR, to, null);
+        toTypes.stream()
+                .map(toType -> new InferredTransfer(method, fromPoint, toPoint, toType, getWeight()))
+                .forEach(result::add);
     }
 
     @Override
