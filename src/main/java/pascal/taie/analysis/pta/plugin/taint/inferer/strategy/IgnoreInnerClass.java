d8708442a85e7ed8a4b0e4e98eb77d639f826270
Yu Wu <yuwu@smail.nju.edu.cn>
2023-08-12 15:55:10 +0800

    Fix TransferInferer

--- src/main/java/pascal/taie/analysis/pta/plugin/taint/inferer/strategy/IgnoreInnerClass.java
+++ src/main/java/pascal/taie/analysis/pta/plugin/taint/inferer/strategy/IgnoreInnerClass.java
@@ -3,19 +3,23 @@ package pascal.taie.analysis.pta.plugin.taint.inferer.strategy;
 import pascal.taie.analysis.pta.core.cs.element.CSCallSite;
 import pascal.taie.analysis.pta.plugin.taint.inferer.InferredTransfer;
 import pascal.taie.language.classes.JClass;
+import pascal.taie.language.type.ClassType;
 
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class IgnoreInnerClass implements TransInferStrategy {
 
+    private boolean isPrivateInnerClass(JClass jClass) {
+        return !jClass.isPublic() && jClass.hasOuterClass();
+    }
+
     @Override
     public Set<InferredTransfer> filter(CSCallSite csCallSite, int index, Set<InferredTransfer> transfers) {
         return transfers.stream()
-                .filter(tf -> {
-                    JClass jClass = tf.getMethod().getDeclaringClass();
-                    return jClass.isPublic() || !jClass.hasOuterClass();
-                })
+                .filter(tf -> !isPrivateInnerClass(tf.getMethod().getDeclaringClass()))
+                .filter(tf -> !(tf.getType() instanceof ClassType classType
+                        && isPrivateInnerClass(classType.getJClass())))
                 .collect(Collectors.toUnmodifiableSet());
     }
 }
