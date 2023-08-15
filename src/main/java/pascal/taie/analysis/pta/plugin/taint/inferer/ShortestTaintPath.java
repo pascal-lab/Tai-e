8bc0a37a4e534b34af9a43c3f5e9866732ebd41f
Yu Wu <yuwu@smail.nju.edu.cn>
2023-08-15 20:58:56 +0800

    Fix bugs in TransferInferer

--- src/main/java/pascal/taie/analysis/pta/plugin/taint/inferer/ShortestTaintPath.java
+++ src/main/java/pascal/taie/analysis/pta/plugin/taint/inferer/ShortestTaintPath.java
@@ -1,11 +1,9 @@
 package pascal.taie.analysis.pta.plugin.taint.inferer;
 
-import pascal.taie.analysis.pta.core.cs.element.CSObj;
 import pascal.taie.analysis.pta.core.cs.element.Pointer;
 import pascal.taie.analysis.pta.core.solver.PointerFlowEdge;
 import pascal.taie.analysis.pta.core.solver.Solver;
-import pascal.taie.analysis.pta.plugin.taint.TaintPointerFlowGraph;
-import pascal.taie.analysis.pta.pts.PointsToSet;
+import pascal.taie.analysis.pta.plugin.taint.TaintObjectFlowGraph;
 import pascal.taie.util.collection.Maps;
 import pascal.taie.util.collection.Sets;
 
@@ -20,17 +18,15 @@ public class ShortestTaintPath {
 
     private static final int INVALID_WEIGHT = Integer.MAX_VALUE;
 
-    private final TaintPointerFlowGraph tpfg;
+    private final TaintObjectFlowGraph tofg;
 
     private final Pointer source;
-
-    private final CSObj concernedObj;
 
     private final ToIntFunction<PointerFlowEdge> weightCalc;
 
     private final ToIntFunction<PointerFlowEdge> costCalc;
 
-    private final PointsToSet pts;
+    private final Solver solver;
 
     private Map<Pointer, Integer> node2PathDistance;
 
@@ -38,20 +34,16 @@ public class ShortestTaintPath {
 
     private Map<Pointer, Integer> node2PathCost;
 
-    public ShortestTaintPath(TaintPointerFlowGraph tpfg,
+    public ShortestTaintPath(TaintObjectFlowGraph tofg,
                              Pointer source,
-                             CSObj concernedObj,
                              ToIntFunction<PointerFlowEdge> weightCalc,
                              ToIntFunction<PointerFlowEdge> costCalc,
                              Solver solver) {
-        assert source.getObjects().contains(concernedObj);
-        this.tpfg = tpfg;
+        this.tofg = tofg;
         this.source = source;
-        this.concernedObj = concernedObj;
         this.weightCalc = weightCalc;
         this.costCalc = costCalc;
-        this.pts = solver.makePointsToSet();
-        pts.addObject(concernedObj);
+        this.solver = solver;
     }
 
     public void compute() {
@@ -66,10 +58,10 @@ public class ShortestTaintPath {
     private void runDijkstra() {
         // Initialize
         PriorityQueue<DistPair> queue = new PriorityQueue<>();
-        Set<Pointer> finished = Sets.newSet(tpfg.getNumberOfNodes());
-        tpfg.forEach(node -> node2PathPredecessor.put(node, null));
-        tpfg.forEach(node -> node2PathDistance.put(node, INVALID_WEIGHT));
-        tpfg.forEach(node -> node2PathCost.put(node, 0));
+        Set<Pointer> finished = Sets.newSet(tofg.getNumberOfNodes());
+        tofg.forEach(node -> node2PathPredecessor.put(node, null));
+        tofg.forEach(node -> node2PathDistance.put(node, INVALID_WEIGHT));
+        tofg.forEach(node -> node2PathCost.put(node, 0));
 
         // Set source node status
         node2PathDistance.put(source, 0);
@@ -82,7 +74,7 @@ public class ShortestTaintPath {
             Pointer minDistNode = distPair.node;
             int minDist = distPair.dist;
             if (finished.add(minDistNode)) {
-                for (PointerFlowEdge outEdge : getOutEdges(minDistNode)) {
+                for (PointerFlowEdge outEdge : tofg.getOutEdgesOf(minDistNode)) {
                     Pointer successor = outEdge.target();
                     if (!finished.contains(successor)) {
                         int newDist = minDist + weightCalc.applyAsInt(outEdge);
@@ -104,15 +96,6 @@ public class ShortestTaintPath {
             }
         }
     }
-
-    private List<PointerFlowEdge> getOutEdges(Pointer pointer) {
-        assert pointer.getObjects().contains(concernedObj);
-        return tpfg.getOutEdgesOf(pointer).stream()
-                .filter(edge -> edge.getTransfers().stream()
-                        .anyMatch(tf -> !tf.apply(edge, pts).isEmpty()))
-                .toList();
-    }
-
 
     /**
      * Get the shortest path from source node to target node.
