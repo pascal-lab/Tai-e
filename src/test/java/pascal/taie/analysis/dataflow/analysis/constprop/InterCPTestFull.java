89df8c307591d068442620061cf9393b5521a8b2
Teng Zhang <zhangt2333@gmail.com>
2023-08-20 11:10:05 +0800

    Enable all interprocedural constant propagation tests

--- src/test/java/pascal/taie/analysis/dataflow/analysis/constprop/InterCPTestFull.java
+++ src/test/java/pascal/taie/analysis/dataflow/analysis/constprop/InterCPTestFull.java
@@ -27,7 +27,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-@Disabled
 public class InterCPTestFull extends InterCPTest {
 
     @ParameterizedTest
