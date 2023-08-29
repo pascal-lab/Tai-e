794b0e8ca56951cc637ef7f0a8c235209d6a2b44
Yu Wu <yuwu@smail.nju.edu.cn>
2023-08-29 16:40:28 +0800

    Fix bugs in TransferGenerator

--- src/main/java/pascal/taie/analysis/pta/plugin/taint/inferer/TransferGenerator.java
+++ src/main/java/pascal/taie/analysis/pta/plugin/taint/inferer/TransferGenerator.java
@@ -1,5 +1,6 @@
 package pascal.taie.analysis.pta.plugin.taint.inferer;
 
+import pascal.taie.analysis.graph.callgraph.CallGraphs;
 import pascal.taie.analysis.graph.callgraph.CallKind;
 import pascal.taie.analysis.graph.callgraph.Edge;
 import pascal.taie.analysis.pta.core.cs.context.Context;
@@ -14,8 +15,8 @@ import pascal.taie.analysis.pta.plugin.taint.TransferPoint;
 import pascal.taie.analysis.pta.plugin.util.InvokeUtils;
 import pascal.taie.analysis.pta.plugin.util.StrategyUtils;
 import pascal.taie.analysis.pta.pts.PointsToSet;
+import pascal.taie.ir.stmt.Invoke;
 import pascal.taie.language.classes.JMethod;
-import pascal.taie.language.type.ClassType;
 import pascal.taie.language.type.ReferenceType;
 import pascal.taie.language.type.Type;
 import pascal.taie.util.collection.Sets;
@@ -40,50 +41,46 @@ public class TransferGenerator {
     public Set<InferredTransfer> getTransfers(CSCallSite csCallSite, int from, int to) {
         CSVar fromVar = getCSVar(csCallSite, from);
         CSVar toVar = getCSVar(csCallSite, to);
+        Set<InferredTransfer> result = Sets.newSet();
         if (fromVar != null && fromVar.getType() instanceof ReferenceType
                 && toVar != null && toVar.getType() instanceof ReferenceType) {
             TransferPoint fromPoint = new TransferPoint(TransferPoint.Kind.VAR, from, null);
             TransferPoint toPoint = new TransferPoint(TransferPoint.Kind.VAR, to, null);
 
-            if(to == InvokeUtils.RESULT) {
-                // collect types in callee method to avoid
-                // r = o.m(..)
-                // r = p
-                Set<InferredTransfer> result = Sets.newSet();
-                for(CSMethod callee : getCalleesOf(csCallSite)) {
-                    Context calleeContext = callee.getContext();
-                    JMethod method = callee.getMethod();
-                    Set<Type> toTypes = method.getIR().getReturnVars()
-                            .stream()
-                            .map(var -> csManager.getCSVar(calleeContext, var))
-                            .map(solver::getPointsToSetOf)
-                            .flatMap(PointsToSet::objects)
-                            .map(CSObj::getObject)
-                            .map(Obj::getType)
-                            .collect(Collectors.toUnmodifiableSet());
-                    for(Type type : toTypes) {
-                        result.add(new InferredTransfer(method, fromPoint, toPoint, type, DEFAULT_WEIGHT));
+            switch (to) {
+                case InvokeUtils.RESULT -> {
+                    // collect types in callee method to avoid
+                    // r = o.m(..)
+                    // r = p
+                    for (CSMethod csMethod : getCalleesOf(csCallSite)) {
+                        JMethod callee = csMethod.getMethod();
+                        for (Type type : getReturnType(csMethod)) {
+                            result.add(new InferredTransfer(callee, fromPoint, toPoint, type, DEFAULT_WEIGHT));
+                        }
                     }
                 }
-                return result;
-            } else {
-                Set<Type> toTypes = getArgType(csCallSite, to);
-                Set<JMethod> callees = getCalleesOf(csCallSite)
-                        .stream()
-                        .map(CSMethod::getMethod)
-                        .collect(Collectors.toUnmodifiableSet());
-                if (!toTypes.isEmpty() && !callees.isEmpty()) {
-                    Set<InferredTransfer> result = Sets.newSet();
-                    for (JMethod callee : callees) {
+                case InvokeUtils.BASE -> {
+                    Invoke callSite = csCallSite.getCallSite();
+                    for(CSObj base : toVar.getObjects()) {
+                        Type baseType = base.getObject().getType();
+                        JMethod callee = CallGraphs.resolveCallee(baseType, callSite);
+                        if(callee != null) {
+                            result.add(new InferredTransfer(callee, fromPoint, toPoint, baseType, DEFAULT_WEIGHT));
+                        }
+                    }
+                }
+                default -> {
+                    Set<Type> toTypes = getArgType(csCallSite, to);
+                    for (CSMethod csMethod : getCalleesOf(csCallSite)) {
+                        JMethod callee = csMethod.getMethod();
                         for (Type toType : toTypes) {
                             result.add(new InferredTransfer(callee, fromPoint, toPoint, toType, DEFAULT_WEIGHT));
                         }
                     }
-                    return result;
                 }
             }
         }
-        return Set.of();
+        return result;
     }
 
     @Nullable
@@ -93,6 +90,19 @@ public class TransferGenerator {
 
     private Set<Type> getArgType(CSCallSite csCallSite, int index) {
         return StrategyUtils.getArgType(solver, csCallSite, index);
+    }
+
+    private Set<Type> getReturnType(CSMethod csMethod) {
+        Context context = csMethod.getContext();
+        JMethod method = csMethod.getMethod();
+        return method.getIR().getReturnVars()
+                .stream()
+                .map(var -> csManager.getCSVar(context, var))
+                .map(solver::getPointsToSetOf)
+                .flatMap(PointsToSet::objects)
+                .map(CSObj::getObject)
+                .map(Obj::getType)
+                .collect(Collectors.toSet());
     }
 
     // Edge with CallKind.OTHER will be filtered
