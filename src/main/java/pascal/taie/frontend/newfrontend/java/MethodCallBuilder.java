51643f17996e64503778fbf8327c42470d4e293c
Chenxi Li <ayanamists@gmail.com>
2023-09-13 13:31:56 +0800

    Use java.lang.Object::clone for array clones

--- src/main/java/pascal/taie/frontend/newfrontend/java/MethodCallBuilder.java
+++ src/main/java/pascal/taie/frontend/newfrontend/java/MethodCallBuilder.java
@@ -4,8 +4,11 @@ import org.eclipse.jdt.core.dom.IMethodBinding;
 import org.eclipse.jdt.core.dom.ITypeBinding;
 import org.eclipse.jdt.core.dom.Modifier;
 import pascal.taie.ir.proginfo.MethodRef;
+import pascal.taie.language.classes.ClassNames;
 import pascal.taie.language.classes.JClass;
 import pascal.taie.language.classes.MethodNames;
+import pascal.taie.language.type.ArrayType;
+import pascal.taie.language.type.ClassType;
 import pascal.taie.language.type.Type;
 import pascal.taie.language.type.VoidType;
 
@@ -18,6 +21,12 @@ public class MethodCallBuilder {
         ITypeBinding declClass = binding.getDeclaringClass();
         JClass jClass = TypeUtils.getTaieClass(declClass);
         Type retType = TypeUtils.JDTTypeToTaieType(binding.getReturnType());
+
+        if (binding.getName().equals("clone") && retType instanceof ArrayType) {
+            ClassType objType = TypeUtils.getClassByName(ClassNames.OBJECT);
+            jClass = objType.getJClass();
+            retType = objType;
+        }
         List<Type> paras = new ArrayList<>();
         for (var i : binding.getParameterTypes()) {
              paras.add(TypeUtils.JDTTypeToTaieType(i));
