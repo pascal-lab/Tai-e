abd946ba9a9c765e086dc2704dea9d1998c17a5b
Teng Zhang <zhangt2333@gmail.com>
2023-11-04 15:33:42 +0800

    Optimize the performance of HybridBitSet

--- src/main/java/pascal/taie/util/collection/HybridBitSet.java
+++ src/main/java/pascal/taie/util/collection/HybridBitSet.java
@@ -24,12 +24,28 @@ package pascal.taie.util.collection;
 
 import pascal.taie.util.Indexer;
 
+import javax.annotation.Nonnull;
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.Set;
 
 /**
  * Hybrid set that uses bit set for large set.
+ * <p>
+ * It is important to note that batch operations such as {@link #addAll(Collection)},
+ * {@link #removeAll(Collection)}, {@link #retainAll(Collection)}, and {@link #containsAll(Collection)}
+ * can experience performance degradation when used with {@link IndexerBitSet}.
+ * <p>
+ * For example, when removing elements of a {@link HybridBitSet} from an {@link IndexerBitSet}
+ * (i.e., {@code indexerBitSet.removeAll(hybridBitSet)}) or vice versa,
+ * the operation does not take advantage of bit-level operations.
+ * Instead, it falls back on an iterative removal process that is less efficient.
+ * <p>
+ * Users are advised to be aware of these potential performance implications when performing
+ * batch operations between {@link IndexerBitSet} and {@link HybridBitSet} instances
+ * to avoid unexpected inefficiencies.
+ *
+ * @see IndexerBitSet
  */
 public final class HybridBitSet<E> extends AbstractHybridSet<E>
         implements Serializable {
@@ -46,6 +62,50 @@ public final class HybridBitSet<E> extends AbstractHybridSet<E>
     @Override
     protected Set<E> newLargeSet(int unused) {
         return new IndexerBitSet<>(indexer, isSparse);
+    }
+
+    @Override
+    public boolean addAll(@Nonnull Collection<? extends E> c) {
+        // optimize when c is HybridBitSet
+        if (this.set instanceof GenericBitSet<E> thisBitSet
+                && c instanceof HybridBitSet<? extends E> other
+                && other.set instanceof GenericBitSet<? extends E> otherBitSet) {
+            return thisBitSet.addAll(otherBitSet);
+        }
+        return super.addAll(c);
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        // optimize when c is HybridBitSet
+        if (this.set instanceof GenericBitSet<E> thisBitSet
+                && c instanceof HybridBitSet<?> other
+                && other.set instanceof GenericBitSet<?> otherBitSet) {
+            return thisBitSet.containsAll(otherBitSet);
+        }
+        return super.containsAll(c);
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        // optimize when c is HybridBitSet
+        if (this.set instanceof GenericBitSet<E> thisBitSet
+                && c instanceof HybridBitSet<?> other
+                && other.set instanceof GenericBitSet<?> otherBitSet) {
+            return thisBitSet.removeAll(otherBitSet);
+        }
+        return super.removeAll(c);
+    }
+
+    @Override
+    public boolean retainAll(@Nonnull Collection<?> c) {
+        // optimize when c is HybridBitSet
+        if (this.set instanceof GenericBitSet<E> thisBitSet
+                && c instanceof HybridBitSet<?> other
+                && other.set instanceof GenericBitSet<?> otherBitSet) {
+            return thisBitSet.retainAll(otherBitSet);
+        }
+        return super.retainAll(c);
     }
 
     @Override
