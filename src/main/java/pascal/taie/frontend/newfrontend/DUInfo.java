28176702cd1d6485391f8ed2b4dc537feb57d93e
Bluesbreaker45 <bluesbreaker45@outlook.com>
2024-01-15 20:23:21 +0800

    Fix the bug caused by the fixed default capacity of ArraySet.

--- src/main/java/pascal/taie/frontend/newfrontend/DUInfo.java
+++ src/main/java/pascal/taie/frontend/newfrontend/DUInfo.java
@@ -1,9 +1,9 @@
 package pascal.taie.frontend.newfrontend;
 
 import pascal.taie.ir.exp.Var;
-import pascal.taie.util.collection.ArraySet;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -16,14 +16,14 @@ public class DUInfo {
     public DUInfo(int varCount) {
         defBlocks = new ArrayList<>(varCount);
         for (int i = 0; i < varCount; i++) {
-            defBlocks.add(new ArraySet<>());
+            defBlocks.add(new HashSet<>());
         }
     }
 
     public void addDefBlock(Var v, IBasicBlock b) {
         int i = v.getIndex();
         while (i >= defBlocks.size()) {
-            defBlocks.add(new ArraySet<>());
+            defBlocks.add(new HashSet<>());
         }
         defBlocks.get(i).add(b);
     }
@@ -32,7 +32,7 @@ public class DUInfo {
         int i = v.getIndex();
         while (i >= defBlocks.size()) {
             // Maybe we can directly return an empty list?
-            defBlocks.add(new ArraySet<>());
+            defBlocks.add(new HashSet<>());
         }
         return defBlocks.get(i);
     }
