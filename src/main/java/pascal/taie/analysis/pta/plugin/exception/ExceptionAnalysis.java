9694d102fe79edef78f9a0c37c0cc8a10062a6da
RacerZ <qiyizhang2002@foxmail.com>
2025-07-02 20:50:55 +0800

    Fix NPE in Exception Analysis
    Co-authored-by: Teng Zhang <zhangt2333@gmail.com>

--- src/main/java/pascal/taie/analysis/pta/plugin/exception/ExceptionAnalysis.java
+++ src/main/java/pascal/taie/analysis/pta/plugin/exception/ExceptionAnalysis.java
@@ -218,8 +218,7 @@ public class ExceptionAnalysis implements Plugin {
             Stmt currentStmt,
             Set<CSObj> newExceptions,
             CSMethod csMethod) {
-        if (!catchers.containsKey(csMethod.getMethod())) return newExceptions;
-        List<ExceptionEntry> entries = catchers.get(csMethod.getMethod())
+        List<ExceptionEntry> entries = catchers.getOrDefault(csMethod.getMethod(), Map.of())
                 .get(currentStmt);
         if (entries != null) {
             Context ctx = csMethod.getContext();
