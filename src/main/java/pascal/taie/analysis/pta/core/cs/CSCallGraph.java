fa1c1ee3180c471f64bc5a68409db0c514cd6f9c
jpwang <jpwang@smail.nju.edu.cn>
2025-08-15 11:35:52 +0800

    Fix missing edges when converting cs callgraph to ci callgraph (#190)

--- src/main/java/pascal/taie/analysis/pta/core/cs/CSCallGraph.java
+++ src/main/java/pascal/taie/analysis/pta/core/cs/CSCallGraph.java
@@ -129,8 +129,15 @@ public class CSCallGraph extends AbstractCallGraph<CSCallSite, CSMethod> {
     @Override
     public Stream<Edge<CSCallSite, CSMethod>> edges() {
         return reachableMethods.stream()
-                .flatMap(this::callSitesIn)
-                .flatMap(this::edgesOutOf);
+                .flatMap(this::edgesInTo);
+    }
+
+    /**
+     * @implNote This implementation may be inefficient, please use with care
+     */
+    @Override
+    public int getNumberOfEdges() {
+        return Math.toIntExact(edges().count());
     }
 
     @Override
