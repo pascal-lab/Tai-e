0b71c0fd41fb9194e28cae044b545dcb3f412723
jpwang <jpwang@smail.nju.edu.cn>
2025-08-21 14:09:30 +0800

    Make `SideEffectAnalysis` use context-sensitive information

--- src/main/java/pascal/taie/analysis/sideeffect/TopologicalSolver.java
+++ src/main/java/pascal/taie/analysis/sideeffect/TopologicalSolver.java
@@ -24,25 +24,26 @@ package pascal.taie.analysis.sideeffect;
 
 import pascal.taie.analysis.graph.callgraph.CallGraph;
 import pascal.taie.analysis.pta.PointerAnalysisResult;
+import pascal.taie.analysis.pta.core.cs.context.Context;
+import pascal.taie.analysis.pta.core.cs.element.CSCallSite;
+import pascal.taie.analysis.pta.core.cs.element.CSMethod;
+import pascal.taie.analysis.pta.core.cs.element.CSObj;
+import pascal.taie.analysis.pta.core.cs.element.CSVar;
 import pascal.taie.analysis.pta.core.heap.Obj;
-import pascal.taie.ir.exp.FieldAccess;
-import pascal.taie.ir.exp.InstanceFieldAccess;
 import pascal.taie.ir.exp.Var;
-import pascal.taie.ir.stmt.Invoke;
 import pascal.taie.ir.stmt.Stmt;
-import pascal.taie.ir.stmt.StoreArray;
-import pascal.taie.ir.stmt.StoreField;
 import pascal.taie.language.classes.JMethod;
 import pascal.taie.util.Indexer;
 import pascal.taie.util.collection.CollectionUtils;
 import pascal.taie.util.collection.IndexerBitSet;
 import pascal.taie.util.collection.Maps;
 import pascal.taie.util.collection.Sets;
+import pascal.taie.util.collection.TwoKeyMap;
 import pascal.taie.util.graph.MergedNode;
 import pascal.taie.util.graph.MergedSCCGraph;
 import pascal.taie.util.graph.TopologicalSorter;
 
-import java.util.Map;
+import java.util.Collection;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -54,64 +55,71 @@ class TopologicalSolver {
 
     private final boolean onlyApp;
 
-    TopologicalSolver(boolean onlyApp) {
+    private final PointerAnalysisResult pta;
+
+    private final CallGraph<CSCallSite, CSMethod> callGraph;
+
+    private final Indexer<Obj> objIndexer;
+
+    TopologicalSolver(boolean onlyApp, PointerAnalysisResult pta) {
         this.onlyApp = onlyApp;
+        this.pta = pta;
+        this.callGraph = pta.getCSCallGraph();
+        this.objIndexer = pta.getObjectIndexer();
     }
 
-    SideEffect solve(PointerAnalysisResult pta) {
-        CallGraph<Invoke, JMethod> callGraph = pta.getCallGraph();
+    SideEffect solve() {
         // 1. compute the objects directly modified by each method and stmt
-        Map<JMethod, Set<Obj>> methodDirectMods = Maps.newMap();
-        Map<Stmt, Set<Obj>> stmtDirectMods = Maps.newMap();
-        computeDirectMods(pta, callGraph, stmtDirectMods, methodDirectMods);
+        TwoKeyMap<JMethod, Context, Set<Obj>> methodDirectMods = Maps.newTwoKeyMap();
+        TwoKeyMap<Stmt, Context, Set<Obj>> stmtDirectMods = Maps.newTwoKeyMap();
+        computeDirectMods(stmtDirectMods, methodDirectMods);
         // 2. compute the objects directly modified by
         //    the methods of each SCC in the call graph
         var mg = new MergedSCCGraph<>(callGraph);
-        Map<JMethod, Set<Obj>> sccDirectMods = computeSCCDirectMods(
+        TwoKeyMap<JMethod, Context, Set<Obj>> sccDirectMods = computeSCCDirectMods(
                 mg.getNodes(), methodDirectMods);
         // 3. fully compute the objects modified by each method
-        Indexer<Obj> indexer = pta.getObjectIndexer();
-        Map<JMethod, Set<Obj>> methodMods = computeMethodMods(
-                mg, callGraph, sccDirectMods, indexer);
-        return new SideEffect(methodMods, stmtDirectMods, callGraph);
+        TwoKeyMap<JMethod, Context, Set<Obj>> methodMods = computeMethodMods(
+                mg, sccDirectMods);
+        return new SideEffect(methodMods, stmtDirectMods, pta.getCallGraph());
     }
 
     private void computeDirectMods(
-            PointerAnalysisResult pta,
-            CallGraph<?, JMethod> callGraph,
-            Map<Stmt, Set<Obj>> stmtDirectMods,
-            Map<JMethod, Set<Obj>> methodDirectMods) {
-        callGraph.forEach(method -> {
-            Set<Obj> mMods = Sets.newHybridSet();
-            method.getIR().forEach(stmt -> {
-                Set<Obj> sMods = Set.of();
-                if (stmt instanceof StoreField storeField) {
-                    FieldAccess fieldAccess = storeField.getFieldAccess();
-                    if (fieldAccess instanceof InstanceFieldAccess instAccess) {
-                        Var base = instAccess.getBase();
-                        sMods = pta.getPointsToSet(base);
-                    }
-                } else if (stmt instanceof StoreArray storeArray) {
-                    Var base = storeArray.getArrayAccess().getBase();
-                    sMods = pta.getPointsToSet(base);
+            TwoKeyMap<Stmt, Context, Set<Obj>> stmtDirectMods,
+            TwoKeyMap<JMethod, Context, Set<Obj>> methodDirectMods) {
+        for (CSVar csVar : pta.getCSVars()) {
+            Context context = csVar.getContext();
+            Var base = csVar.getVar();
+            JMethod method = base.getMethod();
+            Set<Obj> baseObjs = csVar.objects()
+                    .map(CSObj::getObject)
+                    .filter(this::isRelevant)
+                    .collect(Collectors.toUnmodifiableSet());
+            if (!base.getStoreFields().isEmpty()) {
+                for (Stmt stmt : base.getStoreFields()) {
+                    stmtDirectMods.computeIfAbsent(stmt, context,
+                                    (s, c) -> Sets.newSet())
+                            .addAll(baseObjs);
                 }
-                if (!sMods.isEmpty()) {
-                    sMods = sMods.stream()
-                            .filter(this::isRelevant)
-                            .collect(Collectors.toUnmodifiableSet());
-                }
-                if (!sMods.isEmpty()) {
-                    mMods.addAll(sMods);
-                    stmtDirectMods.put(stmt, sMods);
-                }
-            });
-            if (!mMods.isEmpty()) {
-                methodDirectMods.put(method, mMods);
+                methodDirectMods.computeIfAbsent(method, context,
+                                (m, c) -> Sets.newSet())
+                        .addAll(baseObjs);
             }
-        });
+            if (!base.getStoreArrays().isEmpty()) {
+                for (Stmt stmt : base.getStoreArrays()) {
+                    stmtDirectMods.computeIfAbsent(stmt, context,
+                                    (s, c) -> Sets.newSet())
+                            .addAll(baseObjs);
+                }
+                methodDirectMods.computeIfAbsent(method, context,
+                                (m, c) -> Sets.newSet())
+                        .addAll(baseObjs);
+            }
+        }
     }
 
     private boolean isRelevant(Obj obj) {
+        // TODO: this method might not be well-defined for MergedObjs
         if (onlyApp) {
             return obj.getContainerMethod().isPresent()
                     && obj.getContainerMethod().get().isApplication();
@@ -119,46 +127,60 @@ class TopologicalSolver {
         return true;
     }
 
-    private static Map<JMethod, Set<Obj>> computeSCCDirectMods(
-            Set<MergedNode<JMethod>> sccs,
-            Map<JMethod, Set<Obj>> methodDirectMods) {
-        Map<JMethod, Set<Obj>> sccDirectMods = Maps.newMap();
-        sccs.forEach(scc -> {
+    private static TwoKeyMap<JMethod, Context, Set<Obj>> computeSCCDirectMods(
+            Set<MergedNode<CSMethod>> sccs,
+            TwoKeyMap<JMethod, Context, Set<Obj>> methodDirectMods) {
+        TwoKeyMap<JMethod, Context, Set<Obj>> sccDirectMods = Maps.newTwoKeyMap();
+        for (MergedNode<CSMethod> scc : sccs) {
             Set<Obj> mods = Sets.newHybridSet();
-            scc.getNodes().forEach(m ->
-                    mods.addAll(methodDirectMods.getOrDefault(m, Set.of())));
-            scc.getNodes().forEach(m -> sccDirectMods.put(m, mods));
-        });
+            for (CSMethod csMethod : scc.getNodes()) {
+                Context context = csMethod.getContext();
+                JMethod method = csMethod.getMethod();
+                mods.addAll(methodDirectMods.getOrDefault(method, context, Set.of()));
+            }
+            for (CSMethod csMethod : scc.getNodes()) {
+                Context context = csMethod.getContext();
+                JMethod method = csMethod.getMethod();
+                sccDirectMods.put(method, context, mods);
+            }
+        }
         return sccDirectMods;
     }
 
-    private static Map<JMethod, Set<Obj>> computeMethodMods(
-            MergedSCCGraph<JMethod> mg,
-            CallGraph<?, JMethod> callGraph,
-            Map<JMethod, Set<Obj>> sccDirectMods,
-            Indexer<Obj> indexer) {
-        Map<JMethod, Set<Obj>> methodMods = Maps.newMap();
-        // to accelerate side-effect analysis, we propagate modified objects
+    private TwoKeyMap<JMethod, Context, Set<Obj>> computeMethodMods(
+            MergedSCCGraph<CSMethod> mg,
+            TwoKeyMap<JMethod, Context, Set<Obj>> sccDirectMods) {
+        TwoKeyMap<JMethod, Context, Set<Obj>> methodMods = Maps.newTwoKeyMap();
+        // to accelerate side effect analysis, we propagate modified objects
         // of methods (methodMods) based on topological sorting of call graph,
         // so that each method only needs to be processed once
         var sorter = new TopologicalSorter<>(mg, true);
-        sorter.get().forEach(scc -> {
-            Set<Obj> mods = new IndexerBitSet<>(indexer, true);
+        for (MergedNode<CSMethod> scc : sorter.get()) {
+            Set<Obj> mods = new IndexerBitSet<>(objIndexer, true);
             // add SCC direct mods
-            Set<JMethod> sccNodes = Sets.newSet(scc.getNodes());
-            JMethod rep = CollectionUtils.getOne(sccNodes);
-            mods.addAll(sccDirectMods.get(rep));
+            Set<CSMethod> sccNodes = Sets.newSet(scc.getNodes());
+            CSMethod rep = CollectionUtils.getOne(sccNodes);
+            Context repContext = rep.getContext();
+            JMethod repMethod = rep.getMethod();
+            mods.addAll(sccDirectMods.getOrDefault(repMethod, repContext, Set.of()));
             // add callees' mods
-            sccNodes.forEach(m -> callGraph.getCalleesOfM(m)
-                    .stream()
+            sccNodes.stream().map(callGraph::getCalleesOfM).flatMap(Collection::stream)
+                    .distinct()
                     // avoid redundantly adding SCC direct mods
                     .filter(callee -> !sccNodes.contains(callee))
-                    .forEach(callee -> mods.addAll(
-                            methodMods.getOrDefault(callee, Set.of()))));
+                    .forEach(csCallee -> {
+                        Context context = csCallee.getContext();
+                        JMethod callee = csCallee.getMethod();
+                        mods.addAll(methodMods.getOrDefault(callee, context, Set.of()));
+                    });
             if (!mods.isEmpty()) {
-                sccNodes.forEach(m -> methodMods.put(m, mods));
+                for (CSMethod csMethod : sccNodes) {
+                    Context context = csMethod.getContext();
+                    JMethod method = csMethod.getMethod();
+                    methodMods.put(method, context, mods);
+                }
             }
-        });
+        }
         return methodMods;
     }
 }
