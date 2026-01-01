46b4a523e853e72ae7db3d64cd113f85764bedee
chj <506933131@qq.com>
2026-01-01 12:11:32 +0800

    Fix several testcases with unused variables

--- src/test/resources/dataflow/constprop/alias/Interprocedural.java
+++ src/test/resources/dataflow/constprop/alias/Interprocedural.java
@@ -4,6 +4,11 @@ class Interprocedural {
         A a = new A();
         a.f = 555;
         int x = getF(a);
+        if (a.f > 0) {
+            x = getF(a);
+        } else {
+            x = getF(a);
+        }
     }
 
     static int getF(A a) {
