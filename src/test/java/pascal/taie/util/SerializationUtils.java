8dfc810a4079bf440b503811309bae8a61d7bab9
Teng Zhang <zhangt2333@gmail.com>
2026-05-31 20:20:24 +0800

    Simplify serialization copy helper

--- src/test/java/pascal/taie/util/SerializationUtils.java
+++ src/test/java/pascal/taie/util/SerializationUtils.java
@@ -22,12 +22,11 @@
 
 package pascal.taie.util;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.PipedInputStream;
-import java.io.PipedOutputStream;
-import java.util.concurrent.CompletableFuture;
 
 public final class SerializationUtils {
 
@@ -40,24 +39,16 @@ public final class SerializationUtils {
     @SuppressWarnings("unchecked")
     public static <T> T serializedCopy(T o) {
         try {
-            PipedOutputStream pipeOut = new PipedOutputStream();
-            PipedInputStream pipeIn = new PipedInputStream(pipeOut);
-            CompletableFuture.runAsync(() -> {
-                try {
-                    new ObjectOutputStream(pipeOut).writeObject(o);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            });
-            return (T) CompletableFuture.supplyAsync(() -> {
-                try {
-                    return new ObjectInputStream(pipeIn).readObject();
-                } catch (IOException | ClassNotFoundException e) {
-                    throw new RuntimeException(e);
-                }
-            }).get();
-        } catch (Throwable t) {
-            throw new RuntimeException(t);
+            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+            try (ObjectOutputStream out = new ObjectOutputStream(bytes)) {
+                out.writeObject(o);
+            }
+            try (ObjectInputStream in = new ObjectInputStream(
+                    new ByteArrayInputStream(bytes.toByteArray()))) {
+                return (T) in.readObject();
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
 }
